35bedecfa3fca58ea5186508cb63de98518f4790
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2023-12-31 00:06:45 +0000

    added more to the sidebar

--- lib/Screens/SideDrawer.dart
+++ lib/Screens/SideDrawer.dart
@@ -25,12 +25,52 @@ class _SideDrawerState extends State<SideDrawer> {
           ),
           ),
           ListTile(
+            title: Text("Home"),
+            leading: Icon(
+              CupertinoIcons.home,
+              size: 30,
+            ),
+          ),
+          ListTile(
             title: Text("About us "),
             leading: Icon(
-              Icons.person
+              CupertinoIcons.person_fill,
+              size: 30,
+
             ),
             onTap: (){},
-          )
+          ),
+          ListTile(
+            title: Text("Latest News"),
+            leading: Icon(
+              CupertinoIcons.news_solid,
+              size: 30,
+
+            ),
+            onTap: (){},
+          ),
+          ListTile(
+            title: Text("meet Our Teeam "),
+            leading: Icon(
+              CupertinoIcons.group_solid,
+              size: 30,
+            ),
+            onTap: (){},
+          ),
+          ListTile(
+            title: Text("Learning Resources"),
+            leading: Icon(
+              CupertinoIcons.book_solid,
+              size: 30,
+            ),
+          ),
+          ListTile(
+            title: Text("Links"),
+            leading: Icon(
+              CupertinoIcons.link,
+              size: 30,
+            ),
+          ),
         ],
       ),
     );
