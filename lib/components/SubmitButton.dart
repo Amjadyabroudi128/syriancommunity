b9a90dc8f8208585c4feb4a38a9c64c1e9ab9795
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2024-01-04 00:18:11 +0000

    custom button

---
+++ lib/components/SubmitButton.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class CustomButton extends StatelessWidget {
+  final VoidCallback onPressed;
+  final String title;
+  CustomButton({ super.key, required this.onPressed, required this.title});
+
+  @override
+  Widget build(BuildContext context) {
+
+    return MaterialButton(
+        height: 40,
+        color: Color.fromARGB(255, 33, 173, 168),
+        textColor: Colors.white,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+        onPressed: onPressed,
+        child: Text(title));
+    // custom button component to be used when necessary
+  }
+}
