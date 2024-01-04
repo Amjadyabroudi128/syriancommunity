e7da16bc215826b6d876873e4455af859788b372
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2024-01-04 00:16:58 +0000

    add team

--- lib/Screens/team/addTeam.dart
+++ lib/Screens/team/addTeam.dart
@@ -1,5 +1,9 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+
+import '../../components/SubmitButton.dart';
+import '../../components/TextField.dart';
 
 class AddMember extends StatefulWidget {
   const AddMember({Key? key}) : super(key: key);
@@ -9,6 +13,8 @@ class AddMember extends StatefulWidget {
 }
 
 class _AddMemberState extends State<AddMember> {
+  TextEditingController name = TextEditingController();
+  TextEditingController details = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,8 +24,50 @@ class _AddMemberState extends State<AddMember> {
         backgroundColor: Color.fromARGB(255, 33, 173, 168),
       ),
       body: Center(
-        child: Text("This is to add members"),
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(left: 12, bottom: 10),
+                  child: Text("Name"),
+                ),
+                CustomTextForm(
+                    hinttext: "name",
+                    myController: name),
+                SizedBox(height: 20,),
+                Padding(
+                  padding: const EdgeInsets.only(left: 12, bottom: 10),
+                  child: Text("Details"),
+                ),
+                CustomTextForm(
+                    hinttext: "details",
+                    myController: details),
+                SizedBox(height: 15,),
+                Center(
+                  child: CustomButton(
+                      onPressed: () async {
+                        await FirebaseFirestore.instance.collection("members").doc().set(
+                            {
+                              "name" : name.text,
+                              "details" : details.text,
+                            }
+                        );
+                        clearText();
+                      },
+                      title: "Submit"),
+                )
+              ],
+            ),
+          ),
+        ),
       ),
     );
+  }
+  void clearText() {
+    name.clear();
+    details.clear();
   }
 }
