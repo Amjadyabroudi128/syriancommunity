920e9c523fdedc39fbcabffd85d771772b873fbe
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2024-01-05 02:34:38 +0000

    more routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:syrianadmin/Celebrations/CelebrationView.dart';
 import 'package:syrianadmin/Screens/Add%20information.dart';
 import 'package:syrianadmin/Screens/Contact/AddContactDetails.dart';
 import 'package:syrianadmin/Screens/Contact/ContactUS.dart';
@@ -47,6 +48,7 @@ class MyApp extends StatelessWidget {
         "addMember" : (context) => AddMember(),
         "contactus" : (context) => ContactUs(),
         "addcontact" : (context) => AddContactDetails(),
+        "celebrations" : (context) => Celebrations(),
       },
     );
   }
