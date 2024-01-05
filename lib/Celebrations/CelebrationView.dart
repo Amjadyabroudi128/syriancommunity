b9156f3210f7c66d488d566fa9c0be08e2467d60
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2024-01-05 02:35:29 +0000

    celebraton page

---
+++ lib/Celebrations/CelebrationView.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class Celebrations extends StatelessWidget {
+  const Celebrations({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Celebrations"),
+        centerTitle: true,
+        backgroundColor: Color.fromARGB(255, 33, 173, 168),
+      ),
+      body: Center(
+        child: Text("Celebrations"),
+      ),
+    );
+  }
+}
