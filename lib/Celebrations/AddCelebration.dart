877837c084b9e90927f8d1653a733d6accd88c5f
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2024-01-07 00:24:24 +0000

    celebrations

--- lib/Celebrations/AddCelebration.dart
+++ lib/Celebrations/AddCelebration.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+import '../components/TextField.dart';
+
 class AddCelebration extends StatefulWidget {
   const AddCelebration({Key? key}) : super(key: key);
 
@@ -9,11 +11,38 @@ class AddCelebration extends StatefulWidget {
 }
 
 class _AddCelebrationState extends State<AddCelebration> {
+  TextEditingController celebrationName = TextEditingController();
+  TextEditingController celebrationDetail = TextEditingController();
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text("this is a place to add celebrations"),
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text("adding celebrations"),
+        backgroundColor: Color.fromARGB(255, 33, 173, 168),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text("celebration"),
+              ),
+              CustomTextForm(hinttext: "e.g: Christmas", myController: celebrationName),
+              SizedBox(height: 15,),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text("Details"),
+              ),
+              CustomTextForm(hinttext: "what we do ", myController: celebrationName),
+            ],
+          ),
+        ),
       ),
     );
   }
