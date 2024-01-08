b9ab9c598b5021f7dad7f7d0f2d606d5c7034471
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2024-01-08 01:36:42 +0000

    getting data

--- lib/Screens/Contact/ContactUS.dart
+++ lib/Screens/Contact/ContactUS.dart
@@ -3,44 +3,78 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:syrianadmin/components/SubmitButton.dart';
 
-class ContactUs extends StatelessWidget {
-  const ContactUs({Key? key}) : super(key: key);
+class ContactUs extends StatefulWidget {
+  final document;
+  const ContactUs({Key? key, this.document}) : super(key: key);
 
+  @override
+  State<ContactUs> createState() => _ContactUsState();
+}
+
+class _ContactUsState extends State<ContactUs> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Contact us "),
-        backgroundColor: Color.fromARGB(255, 33, 173, 168),
+        title: Text("Contact us"),
+        centerTitle: true,
+         backgroundColor: Color.fromARGB(255, 33, 173, 168),
       ),
       body: Padding(
-        padding: const EdgeInsets.all(15.0),
+        padding: EdgeInsets.all(14.0),
         child: ListView(
           children: [
             CustomButton(
            onPressed: (){
-    Navigator.of(context).pushNamed("addcontact");
-    },
+               Navigator.of(context).pushNamed("addcontact");
+               },
            title: "Add contact details"),
             StreamBuilder(
-              stream: FirebaseFirestore.instance.collection("contact").snapshots(),
-              builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot)
-                {
-                  if (!snapshot.hasData) {
-                    return Center(child: Text("no data yet"));
-                  }
-                  return Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Card(
-                      child: ,
-                    ),
-                  );
-                }
 
             )
           ],
-      ),
+        ),
       ),
     );
   }
 }
+
+// class ContactUs extends StatelessWidget {
+//   ContactUs({required this.document});
+//   final document;
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text("Contact us "),
+//         backgroundColor: Color.fromARGB(255, 33, 173, 168),
+//       ),
+//       body: Padding(
+//         padding: const EdgeInsets.all(15.0),
+//         child: ListView(
+//           children: [
+//             CustomButton(
+//            onPressed: (){
+//     Navigator.of(context).pushNamed("addcontact");
+//     },
+//            title: "Add contact details"),
+//             StreamBuilder(
+//               stream: FirebaseFirestore.instance.collection("contact").snapshots(),
+//               builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot)
+//                 {
+//                   if (!snapshot.hasData) {
+//                     return Center(child: Text("no data yet"));
+//                   }
+//                   return ListView(
+//
+//                   );
+//                 }
+//
+//             )
+//           ],
+//       ),
+//       ),
+//     );
+//   }
+// }
