a83fb2d902100dcb0297804a840b8060dfb36a2f
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2024-01-08 02:38:05 +0000

    started to add data

--- lib/Screens/Contact/ContactUS.dart
+++ lib/Screens/Contact/ContactUS.dart
@@ -5,13 +5,15 @@ import 'package:syrianadmin/components/SubmitButton.dart';
 
 class ContactUs extends StatefulWidget {
   final document;
-  const ContactUs({Key? key, this.document}) : super(key: key);
+  const ContactUs({ @required this.document});
 
   @override
   State<ContactUs> createState() => _ContactUsState();
 }
 
 class _ContactUsState extends State<ContactUs> {
+  final CollectionReference contact =
+  FirebaseFirestore.instance.collection('contact');
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,21 +22,68 @@ class _ContactUsState extends State<ContactUs> {
         centerTitle: true,
          backgroundColor: Color.fromARGB(255, 33, 173, 168),
       ),
-      body: Padding(
-        padding: EdgeInsets.all(14.0),
-        child: ListView(
-          children: [
-            CustomButton(
-           onPressed: (){
-               Navigator.of(context).pushNamed("addcontact");
-               },
-           title: "Add contact details"),
-            StreamBuilder(
+            body: Center(
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child:
+                Column(
+                  children: [
+                    CustomButton(
+                        onPressed: (){
+                          Navigator.of(context).pushNamed("addcontact");
+                        },
+                        title: "Add contact details"),
+                    StreamBuilder <QuerySnapshot>(
+                      stream: FirebaseFirestore.instance.collection("contact").snapshots(),
+                        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+                          if (snapshot.hasError) {
+                            return Text('Something went wrong');
+                          }
 
-            )
-          ],
-        ),
-      ),
+                          if (snapshot.connectionState == ConnectionState.waiting) {
+                            return Text("Loading");
+                          }
+
+                          return ListView(
+                            shrinkWrap: true,
+                            children: snapshot.data!.docs.map((DocumentSnapshot document) {
+                              Map<String, dynamic> data = document.data()! as Map<String, dynamic>;
+                              return ListTile(
+                                title: Text(data['street name']),
+                                subtitle: Text(data['post code']),
+                              );
+                            }).toList(),
+                        );
+                      }
+                    ),
+                  ],
+                ),
+
+              ),
+            ),
+            // StreamBuilder <QuerySnapshot>(
+            //   stream: FirebaseFirestore.instance.collection("contact").snapshots(),
+            //     builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+            //       if (snapshot.hasError) {
+            //         return Text('Something went wrong');
+            //       }
+            //
+            //       if (snapshot.connectionState == ConnectionState.waiting) {
+            //         return Text("Loading");
+            //       }
+            //
+            //       return ListView(
+            //         children: snapshot.data!.docs.map((DocumentSnapshot document) {
+            //           Map<String, dynamic> data = document.data()! as Map<String, dynamic>;
+            //           return ListTile(
+            //             title: Text(data['street name']),
+            //             subtitle: Text(data['post code']),
+            //           );
+            //         }).toList(),
+            //     );
+            //
+            //   }
+            // )
     );
   }
 }
