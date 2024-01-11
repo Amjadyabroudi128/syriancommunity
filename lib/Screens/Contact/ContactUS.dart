fe2f16a51adbd7c681aa0103168274f790fccd7c
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2024-01-11 00:32:03 +0000

    scroll physics

--- lib/Screens/Contact/ContactUS.dart
+++ lib/Screens/Contact/ContactUS.dart
@@ -24,62 +24,117 @@ class _ContactUsState extends State<ContactUs> {
         centerTitle: true,
          backgroundColor: Color.fromARGB(255, 33, 173, 168),
       ),
-            body: SingleChildScrollView(
-              child: Center(
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child:
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Center(
-                        child: CustomButton(
-                            onPressed: (){
-                              Navigator.of(context).pushNamed("addcontact");
-                            },
-                            title: "Add contact details"),
-                      ),
-                      SizedBox(height: 30,),
-                      Text("You can visit us here ", style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),),
-                      StreamBuilder <QuerySnapshot>(
-                        stream: FirebaseFirestore.instance.collection("contact").snapshots(),
-                          builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
-                            if (snapshot.hasError) {
-                              return Text('Something went wrong');
-                            }
+            body: ScrollConfiguration(
+              behavior: ScrollBehavior(),
+              child: SingleChildScrollView(
+                child: Center(
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child:
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Center(
+                          child: CustomButton(
+                              onPressed: (){
+                                Navigator.of(context).pushNamed("addcontact");
+                              },
+                              title: "Add contact details"),
+                        ),
+                        SizedBox(height: 30,),
+                        Text("You can visit us here ", style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),),
+                        StreamBuilder <QuerySnapshot>(
+                          stream: FirebaseFirestore.instance.collection("contact").snapshots(),
+                            builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+                              if (snapshot.hasError) {
+                                return Text('Something went wrong');
+                              }
 
-                            if (snapshot.connectionState == ConnectionState.waiting) {
-                              return Text("Loading");
-                            }
-                            return 
-                              ListView(
-                                scrollDirection: Axis.vertical,
-                                shrinkWrap: true,
-                                children: snapshot.data!.docs.map((DocumentSnapshot document) {
-                                  Map<String, dynamic> data = document.data()! as Map<String, dynamic>;
-                                  return Padding(
-                                    padding: const EdgeInsets.all(8.0),
-                                    child: Column(
-                                      crossAxisAlignment: CrossAxisAlignment.start,
-                                      children: [
-                                        Container(
-                                          width: 400,
-                                          child: Card(
-                                            color: Colors.grey[300],
-                                            child: Padding(
-                                              padding: EdgeInsets.all(12),
+                              if (snapshot.connectionState == ConnectionState.waiting) {
+                                return Text("Loading");
+                              }
+                              return 
+                                ListView(
+                                  scrollDirection: Axis.vertical,
+                                  physics: ScrollPhysics(),
+                                  shrinkWrap: true,
+                                  children: snapshot.data!.docs.map((DocumentSnapshot document) {
+                                    Map<String, dynamic> data = document.data()! as Map<String, dynamic>;
+                                    return Padding(
+                                      padding: const EdgeInsets.all(8.0),
+                                      child: Column(
+                                        crossAxisAlignment: CrossAxisAlignment.start,
+                                        children: [
+                                          Container(
+                                            width: 400,
+                                            child: Card(
+                                              color: Colors.grey[300],
+                                              child: Padding(
+                                                padding: EdgeInsets.all(12),
+                                                child: Column(
+                                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                                  children: [
+                                                            Text("our Address"),
+                                                            SizedBox(height: 10,),
+                                                            Text(data["place"]),
+                                                            SizedBox(height: 10,),
+                                                            Text(data["street name"]),
+                                                            SizedBox(height: 10,),
+                                                            Text(data["city"]),
+                                                            SizedBox(height: 10,),
+                                                            Text(data["post code"]),
+                                                    Row(
+                                                      mainAxisAlignment: MainAxisAlignment.end,
+                                                      children: [
+                                                        IconButton(onPressed: () async{
+                                                          await FirebaseFirestore.instance.collection("contact").doc(document.id).delete();
+                                                          Navigator.of(context).pushNamed("contactus");
+
+                                                        },
+                                                            icon: Icon(Icons.delete, color: Colors.red,)
+                                                        ),
+                                                        IconButton(onPressed: () {
+                                                          Navigator.of(context).push(
+                                                              MaterialPageRoute(builder: (context) =>
+                                                              EditDetails(DocID: document.id,
+                                                                oldPlace: data["place"],
+                                                                oldRoad: data["street name"],
+                                                                oldCity: data["city"],
+                                                                oldEmail: data["email"],
+                                                                oldPHone: data["phone"],
+                                                                oldPostCode: data["post code"],)));
+                                                        },
+                                                            icon: Icon(Icons.edit,)
+                                                        ),
+                                                      ],
+                                                    )
+                                                  ],
+                                                ),
+                                              ),
+                                            ),
+                                          ),
+                                          SizedBox(height: 15,),
+                                          Text("Or you can Contact us through email or phone", style: TextStyle(
+                                              fontSize: 13,
+                                              fontWeight: FontWeight.bold),),
+                                          SizedBox(height: 13,),
+                                          Container(
+                                            height: 200,
+                                            width: 400,
+                                            child: Card(
+                                              color: Colors.grey[300],
                                               child: Column(
                                                 crossAxisAlignment: CrossAxisAlignment.start,
                                                 children: [
-                                                          Text("our Address"),
-                                                          SizedBox(height: 10,),
-                                                          Text(data["place"]),
-                                                          SizedBox(height: 10,),
-                                                          Text(data["street name"]),
-                                                          SizedBox(height: 10,),
-                                                          Text(data["city"]),
-                                                          SizedBox(height: 10,),
-                                                          Text(data["post code"]),
+                                                  Padding(
+                                                    padding: const EdgeInsets.all(12.0),
+                                                    child: Text("Email ${data["email"]}"),
+                                                  ),
+                                                  SizedBox(height: 14,),
+                                                  Padding(
+                                                    padding: const EdgeInsets.all(12.0),
+                                                    child: Text("Ahmad Yabroudi  ${data["phone"]}"),
+                                                  ),
                                                   Row(
                                                     mainAxisAlignment: MainAxisAlignment.end,
                                                     children: [
@@ -93,13 +148,13 @@ class _ContactUsState extends State<ContactUs> {
                                                       IconButton(onPressed: () {
                                                         Navigator.of(context).push(
                                                             MaterialPageRoute(builder: (context) =>
-                                                            EditDetails(DocID: document.id,
-                                                              oldPlace: data["place"],
-                                                              oldRoad: data["street name"],
-                                                              oldCity: data["city"],
-                                                              oldEmail: data["email"],
-                                                              oldPHone: data["phone"],
-                                                              oldPostCode: data["post code"],)));
+                                                                EditDetails(DocID: document.id,
+                                                                  oldPlace: data["place"],
+                                                                  oldRoad: data["street name"],
+                                                                  oldCity: data["city"],
+                                                                  oldEmail: data["email"],
+                                                                  oldPHone: data["phone"],
+                                                                  oldPostCode: data["post code"],)));
                                                       },
                                                           icon: Icon(Icons.edit,)
                                                       ),
@@ -108,71 +163,20 @@ class _ContactUsState extends State<ContactUs> {
                                                 ],
                                               ),
                                             ),
-                                          ),
-                                        ),
-                                        SizedBox(height: 15,),
-                                        Text("Or you can Contact us through email or phone", style: TextStyle(
-                                            fontSize: 13,
-                                            fontWeight: FontWeight.bold),),
-                                        SizedBox(height: 13,),
-                                        Container(
-                                          height: 200,
-                                          width: 400,
-                                          child: Card(
-                                            color: Colors.grey[300],
-                                            child: Column(
-                                              crossAxisAlignment: CrossAxisAlignment.start,
-                                              children: [
-                                                Padding(
-                                                  padding: const EdgeInsets.all(12.0),
-                                                  child: Text("Email ${data["email"]}"),
-                                                ),
-                                                SizedBox(height: 14,),
-                                                Padding(
-                                                  padding: const EdgeInsets.all(12.0),
-                                                  child: Text("Ahmad Yabroudi  ${data["phone"]}"),
-                                                ),
-                                                Row(
-                                                  mainAxisAlignment: MainAxisAlignment.end,
-                                                  children: [
-                                                    IconButton(onPressed: () async{
-                                                      await FirebaseFirestore.instance.collection("contact").doc(document.id).delete();
-                                                      Navigator.of(context).pushNamed("contactus");
+                                          )
+                                        ],
+                                      ),
+                                    );
 
-                                                    },
-                                                        icon: Icon(Icons.delete, color: Colors.red,)
-                                                    ),
-                                                    IconButton(onPressed: () {
-                                                      Navigator.of(context).push(
-                                                          MaterialPageRoute(builder: (context) =>
-                                                              EditDetails(DocID: document.id,
-                                                                oldPlace: data["place"],
-                                                                oldRoad: data["street name"],
-                                                                oldCity: data["city"],
-                                                                oldEmail: data["email"],
-                                                                oldPHone: data["phone"],
-                                                                oldPostCode: data["post code"],)));
-                                                    },
-                                                        icon: Icon(Icons.edit,)
-                                                    ),
-                                                  ],
-                                                )
-                                              ],
-                                            ),
-                                          ),
-                                        )
-                                      ],
-                                    ),
-                                  );
+                                  }).toList(),
+                                );
+                          }
+                        ),
 
-                                }).toList(),
-                              );
-                        }
-                      ),
+                      ],
+                    ),
 
-                    ],
                   ),
-
                 ),
               ),
             ),
