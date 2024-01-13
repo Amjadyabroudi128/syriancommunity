35a76e6e84078c0e3080d70a231934a388f6c2c1
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2024-01-13 00:28:30 +0000

    edit celebrations

---
+++ lib/Screens/Celebrations/editCelebrations.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class EditCelebration extends StatefulWidget {
+  const EditCelebration({Key? key}) : super(key: key);
+
+  @override
+  State<EditCelebration> createState() => _EditCelebrationState();
+}
+
+class _EditCelebrationState extends State<EditCelebration> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
