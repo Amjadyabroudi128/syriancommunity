ec6750d7f3952605fdb9abec92c9b99ebc0e4796
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2024-01-16 00:20:54 +0000

    celebrationview

--- lib/Screens/Celebrations/CelebrationView.dart
+++ lib/Screens/Celebrations/CelebrationView.dart
@@ -1,5 +1,8 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:syrianadmin/Screens/Celebrations/editCelebrations.dart';
 
 import '../../components/SubmitButton.dart';
 class Celebrations extends StatelessWidget {
@@ -13,18 +16,131 @@ class Celebrations extends StatelessWidget {
         centerTitle: true,
         backgroundColor: Color.fromARGB(255, 33, 173, 168),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(15.0),
-        child: ListView(
-          children: [
-            CustomButton(
-                onPressed: (){
-                  Navigator.of(context).pushNamed("addcelebration");
-                },
-                title: "Add celebrations")
-          ],
+      body: SingleChildScrollView(
+        child: Center(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Center(
+                  child: CustomButton(
+                      onPressed: (){
+                        Navigator.of(context).pushNamed("addcelebration");
+                      },
+                      title: "Add celebrations +"),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text("Celebrations ", style: TextStyle(
+                      color: Color.fromARGB(255, 33, 173, 168),
+                      fontWeight: FontWeight.bold, fontSize: 16),
+                  ),
+                ),
+                SizedBox(height: 8,),
+                Container(
+                  width: 400,
+                  height: 100,
+                  child: Card(
+                    color: Colors.grey[200],
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text("join us in our celebrations to bring the community together  throughout the year",
+                      style: TextStyle(fontSize: 16),),
+                    ),
 
-        ),
+                  ),
+                ),
+                StreamBuilder(
+                  stream: FirebaseFirestore.instance.collection("Celebrations").snapshots(),
+                  builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+                    if (snapshot.hasError) {
+                      return Text('Something went wrong');
+                    }
+
+                    if (snapshot.connectionState == ConnectionState.waiting) {
+                      return Text("Loading");
+                    }
+                    return SingleChildScrollView(
+                      child: ListView(
+                        physics: ScrollPhysics(),
+                        shrinkWrap: true,
+                        children: snapshot.data!.docs.map((DocumentSnapshot document){
+                          Map<String, dynamic> data = document.data()! as Map<String, dynamic>;
+                          return Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Padding(
+                                padding: const EdgeInsets.all(13.0),
+                                child: Text(data["name"], style: TextStyle(fontWeight: FontWeight.bold, fontSize: 15,
+                                  color: Color.fromARGB(255, 33, 173, 168),),),
+                              ),
+                              Card(
+                                  child: Image.network(
+                                    data["image"],
+                                    height: 240,
+                                    fit: BoxFit.cover,
+                                    width: MediaQuery.of(context).size.width,
+                                  ),
+                                ),
+                              SizedBox(height: 7,),
+                              Container(
+                                child: Card(
+                                  color: Colors.grey[300],
+                                  elevation: 0,
+                                  child: Column(
+                                    children: [
+                                      Padding(
+                                        padding: const EdgeInsets.all(10.0),
+                                        child: Text(data["details"], style: TextStyle(fontSize: 17),),
+                                      ),
+                                      Row(
+                                        children: [
+                                          IconButton(
+                                            onPressed: (){
+                                              Navigator.of(context).push(
+                                                MaterialPageRoute(
+                                                  builder: (context) =>
+                                                      EditCelebration(
+                                                          DocID: document.id,
+                                                          oldName: data["name"],
+                                                          oldDetail: data["details"],
+                                                          oldUrl: data["image"])
+                                                ),
+                                              );
+
+                                            },
+                                            icon: Icon(
+                                                Icons.edit
+                                            ),
+                                          ),
+                                          IconButton(
+                                            onPressed: ()async {
+                                              await FirebaseFirestore.instance.collection("Celebrations").doc(document.id).delete();
+                                              Navigator.of(context).pushNamed("celebrations");
+                                            },
+                                            icon: Icon(
+                                              Icons.delete,
+                                              color: Colors.red,
+                                            ),
+                                          ),
+                                        ],
+                                        mainAxisAlignment: MainAxisAlignment.center,
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              )
+                            ]
+                          );
+                        },
+                        ).toList(),
+                      ),
+                    );
+                  },
+                )
+              ],
+
+            ),
+          ),
       ),
     );
   }
