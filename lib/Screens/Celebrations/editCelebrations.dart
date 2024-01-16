9cf6e79f1afa0709867ab3b8e74afcd0431db717
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2024-01-16 00:21:15 +0000

    editing celebrations

--- lib/Screens/Celebrations/editCelebrations.dart
+++ lib/Screens/Celebrations/editCelebrations.dart
@@ -1,24 +1,105 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'dart:io';
+import 'package:path/path.dart';
+import 'package:image_picker/image_picker.dart';
+
+import '../../components/SubmitButton.dart';
+import '../../components/TextField.dart';
+
 
 class EditCelebration extends StatefulWidget {
-  const EditCelebration({Key? key}) : super(key: key);
+  final String DocID;
+  final String oldName;
+  final String oldDetail;
+  final String oldUrl;
+
+  const EditCelebration({Key? key, required this.DocID, required this.oldName, required this.oldDetail, required this.oldUrl}) : super(key: key);
 
   @override
   State<EditCelebration> createState() => _EditCelebrationState();
 }
 
 class _EditCelebrationState extends State<EditCelebration> {
+  TextEditingController celebrationName = TextEditingController();
+  TextEditingController celebrationDetails = TextEditingController();
+  String? url;
+  File? file;
+  void initState() {
+    super.initState();
+    celebrationName.text = widget.oldName;
+    celebrationDetails.text = widget.oldDetail;
+    url = widget.oldUrl;
+  }
+  Future pickImage() async {
+    final ImagePicker picker = ImagePicker();
+    final XFile? imageCamera = await picker.pickImage(source: ImageSource.gallery);
+    if (imageCamera != null) {
+      file = File(imageCamera!.path);
+      var imagename = basename(imageCamera!.path);
+      var refStorage = FirebaseStorage.instance.ref(imagename);
+      await refStorage.putFile(file!);
+      url = await refStorage.getDownloadURL();
+    }
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
-        title: Text("edit celebrations "),
+        title: Text("adding celebrations"),
         backgroundColor: Color.fromARGB(255, 33, 173, 168),
       ),
-      body: Center(
-        child: Text("this is the editing page "),
+      body: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text("celebration"),
+              ),
+              CustomTextForm(hinttext: "e.g: Christmas", myController: celebrationName),
+              SizedBox(height: 15,),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text("Details"),
+              ),
+              CustomTextForm(hinttext: "what we do ", myController: celebrationDetails,
+                maxLines: 7,),
+              SizedBox(height: 15,),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Center(
+                  child: CustomButton(
+                    title: "celebration image",
+                    onPressed: () async {
+                      await pickImage();
+                      setState(() {
+                      });
+                    },
+                  ),
+                ),
+              ),
+              Center(
+                child: CustomButton(
+                    onPressed: () async{
+                      await FirebaseFirestore.instance.collection("Celebrations").doc(widget.DocID).update(
+                          {
+                            "image" : url,
+                            "name" : celebrationName.text,
+                            "details" : celebrationDetails.text
+                          }
+                      );
+                      Navigator.of(context).pushNamed("celebrations");
+                    }, title: "Edit"),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
