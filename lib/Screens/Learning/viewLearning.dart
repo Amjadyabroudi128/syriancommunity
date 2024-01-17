e62e5eba1ba433aca57db6eb7e2638ce770c948e
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2024-01-17 00:56:28 +0000

    view

---
+++ lib/Screens/Learning/viewLearning.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class LearningResources extends StatefulWidget {
+  const LearningResources({Key? key}) : super(key: key);
+
+  @override
+  State<LearningResources> createState() => _LearningResourcesState();
+}
+
+class _LearningResourcesState extends State<LearningResources> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
