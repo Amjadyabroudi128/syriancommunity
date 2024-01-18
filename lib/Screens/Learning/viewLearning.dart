e6c4d271717f0c7ef419bc63ac9b803f6f28bca0
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2024-01-18 00:26:33 +0000

    view learning page

--- lib/Screens/Learning/viewLearning.dart
+++ lib/Screens/Learning/viewLearning.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:syrianadmin/components/SubmitButton.dart';
 
 class LearningResources extends StatefulWidget {
   const LearningResources({Key? key}) : super(key: key);
@@ -16,6 +17,22 @@ class _LearningResourcesState extends State<LearningResources> {
         centerTitle: true,
         title: Text("Learning resources"),
         backgroundColor: Color.fromARGB(255, 33, 173, 168),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Center(
+                child: CustomButton(
+                    onPressed: (){
+                      Navigator.of(context).pushNamed("addLearning");
+                    },
+                    title: "add learning "),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
