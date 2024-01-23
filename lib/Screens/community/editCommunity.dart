f90762d1b524aed57609f698cbe590582f47b69f
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2024-01-23 00:15:51 +0000

    edit

--- lib/Screens/community/editCommunity.dart
+++ lib/Screens/community/editCommunity.dart
@@ -1,5 +1,15 @@
+import 'dart:io';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:path/path.dart';
+
+import '../../components/SubmitButton.dart';
+import '../../components/TextField.dart';
+
 
 class EditCommunity extends StatefulWidget {
   final String DocID;
@@ -14,6 +24,27 @@ class EditCommunity extends StatefulWidget {
 }
 
 class _EditCommunityState extends State<EditCommunity> {
+  TextEditingController name = TextEditingController();
+  TextEditingController details = TextEditingController();
+  String? url;
+  File? file;
+  void initState() {
+    super.initState();
+    name.text = widget.oldName;
+    details.text = widget.oldDetails;
+    url = widget.oldUrl;
+  }
+  Future pickImage() async {
+    final ImagePicker picker = ImagePicker();
+    final XFile? imageCamera = await picker.pickImage(source: ImageSource.gallery);
+    if (imageCamera != null) {
+      file = File(imageCamera!.path);
+      var imagename = basename(imageCamera!.path);
+      var refStorage = FirebaseStorage.instance.ref(imagename);
+      await refStorage.putFile(file!);
+      url = await refStorage.getDownloadURL();
+    }
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,6 +52,62 @@ class _EditCommunityState extends State<EditCommunity> {
         title: Text("Edit community "),
         centerTitle: true,
         backgroundColor: Color.fromARGB(255, 33, 173, 168),
+      ),
+      body: Center(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(left: 12, bottom: 10),
+                  child: Text("Name"),
+                ),
+                CustomTextForm(
+                    hinttext: "name",
+                    myController: name),
+                SizedBox(height: 20,),
+                Padding(
+                  padding: const EdgeInsets.only(left: 12, bottom: 10),
+                  child: Text("Details"),
+                ),
+                CustomTextForm(
+                    maxLines: 5,
+                    hinttext: "details",
+                    myController: details),
+                SizedBox(height: 12,),
+                Center(
+                  child: CustomButton(
+                    title: "get image",
+                    onPressed: () async {
+                      await pickImage();
+                      setState(() {
+                      });
+                    },
+                  ),
+                ),
+                // if (url != null) Image.network("https://firebasestorage.googleapis.com/v0/b/syriancommunity-5239d.appspot.com/o/Fathi_Khalil.jpg?alt=media&token=5b3ea083-6c67-499c-b528-33c46cffe528"),
+                SizedBox(height: 15,),
+                Center(
+                  child: CustomButton(
+                      onPressed: () async {
+                        await FirebaseFirestore.instance.collection("Community").doc(widget.DocID).update(
+                            {
+                              "image" : url,
+                              "name" : name.text,
+                              "details" : details.text
+                            }
+                        );
+                        Navigator.of(context).pushNamed("community");
+
+                      },
+                      title: "Edit"),
+                )
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
