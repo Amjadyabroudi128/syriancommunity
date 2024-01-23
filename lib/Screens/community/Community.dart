cf52a1e96080d88bf92a885e634dec4f9e3f3ace
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2024-01-23 00:47:16 +0000

    community page

--- lib/Screens/community/Community.dart
+++ lib/Screens/community/Community.dart
@@ -1,10 +1,13 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:syrianadmin/Screens/community/editCommunity.dart';
 import 'package:syrianadmin/components/SubmitButton.dart';
 
 class Community extends StatefulWidget {
-  const Community({Key? key}) : super(key: key);
+  final document;
+
+  const Community({Key? key, this.document}) : super(key: key);
 
   @override
   State<Community> createState() => _CommunityState();
@@ -74,13 +77,54 @@ class _CommunityState extends State<Community> {
                           color: Colors.grey[300],
                           child: Column(
                             children: [
-                              Padding(
-                                padding: const EdgeInsets.all(10.0),
-                                child: Text(data["details"], style: TextStyle(fontSize: 17),),
-                              ),
-                              Padding(
-                                padding: const EdgeInsets.only(left: 310),
-                              ),
+                                Padding(
+                                  padding: const EdgeInsets.only(top: 2),
+                                  child: Text(data["details"], style: TextStyle(fontSize: 17),),
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.only(left: 310),
+                                  child: PopupMenuButton(
+                                    iconSize: 30,
+                                    // add icon, by default "3 dot" icon
+                                    // icon: Icon(Icons.book)
+                                    itemBuilder: (context){
+                                      return [
+                                        PopupMenuItem<int>(
+                                            value: 0,
+                                            child:Icon(Icons.edit)
+                                        ),
+
+                                        PopupMenuItem<int>(
+                                          value: 1,
+                                          child: Icon(Icons.delete, color: Colors.red,),
+                                        ),
+                                      ];
+                                    },
+                                    onSelected:(value){
+                                      if(value == 0){
+                                        // Navigator.of(context).push(
+                                        //     MaterialPageRoute(builder: (context) =>
+                                        //         EditCommunity(DocID: document.id ,
+                                        //           oldName: data["name"],
+                                        //           oldUrl: data["image"],
+                                        //           oldDetails: data["details"],)
+                                        //     ));
+                                        Navigator.of(context).push(
+                                          MaterialPageRoute(
+                                            builder: (context) => EditCommunity(
+                                                DocID: document.id,
+                                                oldName: data["name"],
+                                                oldDetails: data["details"],
+                                                // oldUrl: data["image"])
+                                          )
+                                        ));
+                                      }else if(value == 1){
+                                        FirebaseFirestore.instance.collection("Community").doc(document.id).delete();
+                                        Navigator.of(context).pushNamed("community");
+                                      }
+                                    },
+                                  ),
+                                ),
                             ],
                           ),
                         ),
