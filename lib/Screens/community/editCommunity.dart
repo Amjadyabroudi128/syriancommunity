6c19c414aa2fd05a7e2fbb10ed0ea33c07a475fa
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2024-01-23 07:43:57 +0000

    edit community is back

---
+++ lib/Screens/community/editCommunity.dart
@@ -0,0 +1,115 @@
+import 'dart:io';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:path/path.dart';
+
+import '../../components/SubmitButton.dart';
+import '../../components/TextField.dart';
+
+class EditCommunity extends StatefulWidget {
+  final String DocID;
+  final String oldName;
+  final String oldDetails;
+  final String? oldUrl;
+  const EditCommunity({Key? key, required this.DocID, required this.oldName, required this.oldDetails, this.oldUrl,}) : super(key: key);
+
+  @override
+  State<EditCommunity> createState() => _EditCommunityState();
+}
+
+class _EditCommunityState extends State<EditCommunity> {
+  TextEditingController name = TextEditingController();
+  TextEditingController details = TextEditingController();
+  String? url;
+  File? file;
+  void initState() {
+    super.initState();
+    name.text = widget.oldName;
+    details.text = widget.oldDetails;
+    url = widget.oldUrl;
+  }
+  Future pickImage() async {
+    final ImagePicker picker = ImagePicker();
+    final XFile? imageCamera = await picker.pickImage(source: ImageSource.gallery);
+    if (imageCamera != null) {
+      file = File(imageCamera!.path);
+      var imagename = basename(imageCamera!.path);
+      var refStorage = FirebaseStorage.instance.ref(imagename);
+      await refStorage.putFile(file!);
+      url = await refStorage.getDownloadURL();
+    }
+  }
+  final CollectionReference community =
+  FirebaseFirestore.instance.collection('Community');
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text("adding Community Details"),
+        backgroundColor: Color.fromARGB(255, 33, 173, 168),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(12),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Center(child: Text("Add Community info ")),
+              SizedBox(height: 12,),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text("name"),
+              ),
+              CustomTextForm(hinttext: "BreakFast Club", myController: name),
+              SizedBox(height: 12,),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text("Details"),
+              ),
+              CustomTextForm(hinttext: "What we do ", myController: details, maxLines: 6,),
+              SizedBox(height: 15,),
+              Center(
+                child: CustomButton(
+                  title: "get image",
+                  onPressed: () async {
+                    await pickImage();
+                    setState(() {
+
+                    });
+                  },
+                ),
+              ),
+              Center(
+                child: CustomButton(onPressed: () async {
+                  if(url == null) {
+                  await community.doc(widget.DocID).update({
+                      "name" : name.text,
+                      "details" : details.text
+                    });
+                  } else {
+                  await community.doc(widget.DocID).update({
+                      "name" : name.text,
+                      "details" : details.text,
+                      "image" : url
+                    });
+                  Navigator.of(context).pushNamed("community");
+
+                  }
+                }
+                  , title: "edit",
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+
