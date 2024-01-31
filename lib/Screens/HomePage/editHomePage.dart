b378bdeeb7691d0f1b147a3e91700342ec8e004b
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2024-01-31 08:44:38 +0000

    TextFields and submit button

--- lib/Screens/HomePage/editHomePage.dart
+++ lib/Screens/HomePage/editHomePage.dart
@@ -25,6 +25,51 @@ class _EditHomeState extends State<EditHome> {
       appBar: AppBar(
         backgroundColor: Color.fromARGB(255, 33, 173, 168),
       ),
+      body: Center(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(left: 12, bottom: 10),
+                  child: Text("Name"),
+                ),
+                CustomTextForm(
+                    hinttext: "name",
+                    myController: name),
+                SizedBox(height: 20,),
+                Padding(
+                  padding: const EdgeInsets.only(left: 12, bottom: 10),
+                  child: Text("Details"),
+                ),
+                CustomTextForm(
+                  hinttext: "details",
+                  myController: details,
+                  maxLines: 6,
+                ),
+                SizedBox(height: 15,),
+                Center(
+                  child: CustomButton(
+                      onPressed: () async {
+                        await FirebaseFirestore.instance.collection("home").doc().set(
+                            {
+                              "name" : name.text,
+                              "details" : details.text,
+                              "time" : DateTime.now(),
+                            }
+                        );
+                        clearText();
+                        Navigator.of(context).pushNamed("homepage");
+                      },
+                      title: "Submit"),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),,
     );
   }
 }
