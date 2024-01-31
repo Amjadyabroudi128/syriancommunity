f96eb078beff00e87e57e7ee317b9f3a7db0e30c
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2024-01-31 08:09:09 +0000

    edit home page

---
+++ lib/Screens/HomePage/editHomePage.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class EditHome extends StatefulWidget {
+  const EditHome({Key? key}) : super(key: key);
+
+  @override
+  State<EditHome> createState() => _EditHomeState();
+}
+
+class _EditHomeState extends State<EditHome> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: ,
+    );
+  }
+}
