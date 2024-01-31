f87634d17a8f1240e23e6ec0d24dbb9f9486afca
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2024-01-31 08:11:43 +0000

    edit home page

--- lib/Screens/HomePage/editHomePage.dart
+++ lib/Screens/HomePage/editHomePage.dart
@@ -2,17 +2,24 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class EditHome extends StatefulWidget {
-  const EditHome({Key? key}) : super(key: key);
+  final String DocID;
+  final String oldName;
+  final String oldDetail;
+  const EditHome({Key? key, required this.DocID, required this.oldName, required this.oldDetail}) : super(key: key);
 
   @override
   State<EditHome> createState() => _EditHomeState();
 }
 
 class _EditHomeState extends State<EditHome> {
+  TextEditingController name = TextEditingController();
+  TextEditingController details = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: ,
+      appBar: AppBar(
+        backgroundColor: Color.fromARGB(255, 33, 173, 168),
+      ),
     );
   }
 }
