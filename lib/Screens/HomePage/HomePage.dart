5270cdf9e3397aa53db412ed03447f90934f5cd7
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2024-02-04 08:18:46 +0000

    Current user

--- lib/Screens/HomePage/HomePage.dart
+++ lib/Screens/HomePage/HomePage.dart
@@ -1,4 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
@@ -24,6 +25,8 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     final CollectionReference home =
     FirebaseFirestore.instance.collection('home');
+    User? user = FirebaseAuth.instance.currentUser;
+
     return Scaffold(
       appBar: AppBar(
         leading: Builder(
