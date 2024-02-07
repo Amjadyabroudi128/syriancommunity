3239f87052a1c1a95b1696afa84a328fd2e24f64
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2024-02-07 15:17:29 +0000

    Auth Cubit works

--- lib/Register/LogIn.dart
+++ lib/Register/LogIn.dart
@@ -2,6 +2,8 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:syrianadmin/Cubits/auth_cubit.dart';
 import 'package:syrianadmin/components/TextField.dart';
 
 class Login extends StatefulWidget {
@@ -18,6 +20,13 @@ class _LoginState extends State<Login> {
   String testPassword = "Syria963";
   @override
   Widget build(BuildContext context) {
+    return BlocConsumer<AuthCubit, AuthState>(
+  listener: (context, state) {
+    if (state is LoginSuccess) {
+      Navigator.of(context).pushNamed("homepage");
+    }
+  },
+  builder: (context, state) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Color.fromARGB(255, 33, 173, 168),
@@ -32,11 +41,11 @@ class _LoginState extends State<Login> {
             SizedBox(
               height: 15,
             ),
-                        CustomTextForm(hinttext: "email",),
+            CustomTextForm(hinttext: "email",),
             SizedBox(
               height: 13,
             ),
-                       CustomTextForm(hinttext: "pass",),
+            CustomTextForm(hinttext: "pass",),
             SizedBox(
               height: 15,
             ),
@@ -51,10 +60,11 @@ class _LoginState extends State<Login> {
                   backgroundColor: Colors.blue,
                   padding: const EdgeInsets.symmetric(vertical: 14),
                 ),
-                onPressed: () async {
+                onPressed: ()  {
                   // await FirebaseAuth.instance.signInWithEmailAndPassword(
                   //     email: testEmail, password: testPassword);
-                  Navigator.of(context).pushNamed("homepage");
+                  // Navigator.of(context).pushNamed("homepage");
+                  BlocProvider.of<AuthCubit>(context).login(email: testEmail, password: testPassword);
                 },
                 child: const Text(
                   'Login',
@@ -66,5 +76,7 @@ class _LoginState extends State<Login> {
         ),
       ),
     );
+  },
+);
   }
 }
