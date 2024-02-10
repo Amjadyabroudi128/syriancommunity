8de96367fc1697ebf8a513f810df76dc6f5a99f4
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2024-02-10 00:28:40 +0000

    firebase messaging

--- lib/Screens/HomePage/AddHomePage.dart
+++ lib/Screens/HomePage/AddHomePage.dart
@@ -1,4 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
@@ -63,6 +64,7 @@ class _AddInfoState extends State<AddInfo> {
                        );
                        clearText();
                        await FirebaseApi().initNotifications();
+                       // await FirebaseMessaging.instance.subscribeToTopic("topic");
                        Navigator.of(context).pushNamed("homepage");
                       },
                       title: "Submit"),
