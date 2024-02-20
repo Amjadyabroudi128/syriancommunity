2f337d949271dfcb0b4fd865afbaaa37744e441a
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2024-02-20 00:30:12 +0000

    added flag emoji

--- lib/classes/language.dart
+++ lib/classes/language.dart
@@ -1,12 +1,13 @@
  class language  {
   final String name;
+  final String flag;
   final String languageCode;
-  language (this.name, this.languageCode);
+  language (this.name, this.languageCode, this.flag);
 
   static List<language> languageList() {
     return <language> [
-      language("English", "en"),
-      language("Arabic", "ar"),
+      language("English", "en", "🇬🇧"),
+      language("Arabic", "ar", "🇸🇦")
     ];
   }
 
