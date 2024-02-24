1d8bd820079dc42f860f9901b4b1223de7261429
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2024-02-24 04:05:03 +0000

    removed as it can't be null

--- lib/Screens/community/editCommunity.dart
+++ lib/Screens/community/editCommunity.dart
@@ -35,8 +35,8 @@ class _EditCommunityState extends State<EditCommunity> {
     final ImagePicker picker = ImagePicker();
     final XFile? imageCamera = await picker.pickImage(source: ImageSource.gallery);
     if (imageCamera != null) {
-      file = File(imageCamera!.path);
-      var imagename = basename(imageCamera!.path);
+      file = File(imageCamera.path);
+      var imagename = basename(imageCamera.path);
       var refStorage = FirebaseStorage.instance.ref(imagename);
       await refStorage.putFile(file!);
       url = await refStorage.getDownloadURL();
