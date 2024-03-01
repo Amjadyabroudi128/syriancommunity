c67eb71880640a28dbe71cdeeb703d24ba547305
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2024-03-01 00:39:45 +0000

    const

--- lib/components/SubmitButton.dart
+++ lib/components/SubmitButton.dart
@@ -4,14 +4,14 @@ import 'package:flutter/material.dart';
 class CustomButton extends StatelessWidget {
   final VoidCallback onPressed;
   final String title;
-  CustomButton({ super.key, required this.onPressed, required this.title});
+  const CustomButton({ super.key, required this.onPressed, required this.title});
 
   @override
   Widget build(BuildContext context) {
 
     return MaterialButton(
         height: 40,
-        color: Color.fromARGB(255, 33, 173, 168),
+        color: const Color.fromARGB(255, 33, 173, 168),
         textColor: Colors.white,
         shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
         onPressed: onPressed,
