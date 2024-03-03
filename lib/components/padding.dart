10139e7a9c8700f0d31195963485e067d3be33cf
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2024-03-03 03:22:01 +0000

    padding class

---
+++ lib/components/padding.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/cupertino.dart';
+
+class padding extends StatelessWidget {
+  final Widget? child;
+  const padding({super.key, required this.child});
+
+  @override
+  Widget build(BuildContext context) {
+    return  Padding(
+      padding: EdgeInsets.all(10),
+      child: child,
+    );
+  }
+}
