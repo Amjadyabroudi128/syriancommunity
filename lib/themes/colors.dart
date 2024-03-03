c9a9b09e17baa4d862b4c08d46bc4f0dc1659945
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2024-03-03 03:19:08 +0000

    color change

--- lib/themes/colors.dart
+++ lib/themes/colors.dart
@@ -3,6 +3,6 @@ import 'package:flutter/material.dart';
 
 class ColorManager {
   static Color specialGreen = Color.fromARGB(255, 33, 173, 168);
-  static Color cardColor = Color(0xEEEEEEFF);
+  static Color cardColor = Color(0xFFEEEEEE);
   static Color buttonColor = Colors.blue;
 }
