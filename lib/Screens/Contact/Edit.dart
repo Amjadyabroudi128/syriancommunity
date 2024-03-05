dbe4e197405aefe040977ed163d89622536d36da
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2024-03-05 00:06:05 +0000

    centre title and BG color

--- lib/Screens/Contact/Edit.dart
+++ lib/Screens/Contact/Edit.dart
@@ -47,9 +47,7 @@ class _AddContactDetailsState extends State<EditDetails> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        centerTitle: true,
         title: Text(AppLocalizations.of(context)!.editDetails),
-        backgroundColor: Color.fromARGB(255, 33, 173, 168),
       ),
       body: Padding(
         padding: const EdgeInsets.all(12.0),
