1b7b2912bb19db564653f587d28424e65a721890
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2024-03-08 01:23:24 +0000

    app theme class

---
+++ lib/themes/app_theme.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'colors.dart';
+
+ ThemeData appTheme = ThemeData(
+     useMaterial3: false,
+     appBarTheme: AppBarTheme(
+       color: ColorManager.specialGreen,
+       centerTitle: true,
+     )
+ );
