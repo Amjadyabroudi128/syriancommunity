be81bfeaf820b20bb339c4cb5bf1d88949041773
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2024-03-08 01:19:41 +0000

    date color

--- lib/themes/colors.dart
+++ lib/themes/colors.dart
@@ -5,4 +5,5 @@ class ColorManager {
   static Color specialGreen = Color.fromARGB(255, 33, 173, 168);
   static Color cardColor = Color(0xFFEEEEEE);
   static Color buttonColor = Colors.blue;
+  static Color dateColor = Color(0xFF9E9E9E);
 }
