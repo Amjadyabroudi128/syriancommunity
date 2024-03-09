1dd2f2f78162536862271e1554a9b1bbc4654f65
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2024-03-09 00:05:40 +0000

    font weight

--- lib/themes/font_weight_helper.dart
+++ lib/themes/font_weight_helper.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
 class FontWeightHelper {
   static const FontWeight bold = FontWeight.w700;
+  static FontWeight semiBold = FontWeight.w600;
+  static FontWeight extraBold = FontWeight.w800;
+
 }
