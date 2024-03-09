bd7e29ef445af0a171832baf9bd553d161d71310
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2024-03-09 00:25:24 +0000

    changed button color

--- lib/themes/colors.dart
+++ lib/themes/colors.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart';
 class ColorManager {
   static Color specialGreen = Color.fromARGB(255, 33, 173, 168);
   static Color cardColor = Color(0xFFEEEEEE);
-  static Color buttonColor = Colors.blue;
+  static Color buttonColor = Color.fromARGB(255, 33, 173, 168);
   static Color dateColor = Color(0xFF9E9E9E);
   static Color hintText = Color(0xFF000000);
 }
