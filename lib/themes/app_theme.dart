5cc39b750272a87929c2a92b766a79026612e30c
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2024-03-09 00:04:47 +0000

    input decoration theme

--- lib/themes/app_theme.dart
+++ lib/themes/app_theme.dart
@@ -6,5 +6,22 @@ import 'colors.dart';
      appBarTheme: AppBarTheme(
        color: ColorManager.specialGreen,
        centerTitle: true,
-     )
+     ),
+   inputDecorationTheme: InputDecorationTheme(
+     // contentPadding: EdgeInsets.all(30),
+       border: OutlineInputBorder(
+         borderRadius: BorderRadius.circular(30),
+         borderSide: BorderSide(color: ColorManager.hintText),
+       ),
+     enabledBorder: OutlineInputBorder(
+       borderSide: BorderSide(width: 1, color: ColorManager.hintText), //<-- SEE HERE
+       borderRadius: BorderRadius.circular(50.0),
+     ),
+       focusedBorder: OutlineInputBorder(
+           borderRadius: BorderRadius.circular(30),
+           borderSide:  BorderSide(
+                color: ColorManager.hintText
+           )
+       )
+   ),
  );
