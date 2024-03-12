d88c72c9ec49eaa431653a2d8ebec43d8b5f52dc
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2024-03-12 15:24:39 +0000

    Cupertino Icons

--- lib/Screens/SideDrawer.dart
+++ lib/Screens/SideDrawer.dart
@@ -3,7 +3,6 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:syrianadmin/Register/LogIn.dart';
-import 'package:url_launcher/url_launcher.dart';
 import '../main.dart';
 
 
@@ -124,7 +123,7 @@ class _SideDrawerState extends State<SideDrawer> {
           ),
           ExpansionTile(
               leading: Icon(
-                Icons.language
+                CupertinoIcons.globe,
               ),
               title: Text(AppLocalizations.of(context)!.language),
 
