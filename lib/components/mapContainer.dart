fd7829f3f6e537c7473d373164cb8eebf5cd6782
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2024-03-13 01:25:55 +0000

    map container

---
+++ lib/components/mapContainer.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+
+class MyLocation extends StatelessWidget {
+  const MyLocation({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
