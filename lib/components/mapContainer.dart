b76947da1792024ffd144237fbdb93570c480866
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2024-03-13 01:27:02 +0000

    myLocation Component

--- lib/components/mapContainer.dart
+++ lib/components/mapContainer.dart
@@ -1,10 +1,30 @@
 import 'package:flutter/cupertino.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class MyLocation extends StatelessWidget {
   const MyLocation({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Container(
+      height: 300,
+      width: 300,
+      child: GoogleMap(
+        initialCameraPosition: CameraPosition(
+          target: LatLng(50.82062318563144, -0.12208351759729541),
+          zoom: 14,
+        ),
+        markers: {
+          Marker(
+              markerId: MarkerId("college"),
+              position: LatLng(50.82062318563144, -0.12208351759729541),
+              draggable: true,
+              onDragEnd: (value) {
+
+              }
+          )
+        },
+      ),
+    );
   }
 }
