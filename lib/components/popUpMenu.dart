c120063ce4dd31bc9bb49a3e3af830739ef3172d
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2024-03-16 00:09:25 +0000

    iconsize 30

--- lib/components/popUpMenu.dart
+++ lib/components/popUpMenu.dart
@@ -3,15 +3,15 @@ import 'package:flutter/material.dart';
 class MyPopUpMenu extends StatelessWidget {
   final PopupMenuItemBuilder itemBuilder;
   final PopupMenuItemSelected onSelected;
-  final double iconSize;
-  const MyPopUpMenu({super.key, required this.itemBuilder, required this.onSelected, required this.iconSize});
+  final double? iconSize;
+  const MyPopUpMenu({super.key, required this.itemBuilder, required this.onSelected, this.iconSize});
 
   @override
   Widget build(BuildContext context) {
     return PopupMenuButton(
       itemBuilder: itemBuilder,
       onSelected: onSelected,
-      iconSize: iconSize,
+      iconSize: 30,
     );
   }
 }
