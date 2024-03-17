34f27fc703af5579e5cf39f221afcd8088301d67
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2024-03-17 05:11:41 +0000

    removed icon size from popUpMenu.dart

--- lib/components/popUpMenu.dart
+++ lib/components/popUpMenu.dart
@@ -3,15 +3,13 @@ import 'package:flutter/material.dart';
 class MyPopUpMenu extends StatelessWidget {
   final PopupMenuItemBuilder itemBuilder;
   final PopupMenuItemSelected onSelected;
-  final double? iconSize;
-  const MyPopUpMenu({super.key, required this.itemBuilder, required this.onSelected, this.iconSize});
+  const MyPopUpMenu({super.key, required this.itemBuilder, required this.onSelected, });
 
   @override
   Widget build(BuildContext context) {
     return PopupMenuButton(
       itemBuilder: itemBuilder,
       onSelected: onSelected,
-      iconSize: 30,
     );
   }
 }
