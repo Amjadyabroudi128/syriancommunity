e05edacd1d27d72fbf9218497475bdd22f4ae191
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2024-03-17 05:40:36 +0000

    reverted

--- lib/components/SubmitButton.dart
+++ lib/components/SubmitButton.dart
@@ -4,14 +4,19 @@ class CustomButton extends StatelessWidget {
   final VoidCallback onPressed;
   final String title;
   final Color? color;
-  final ButtonStyle? style;
-  const CustomButton({ super.key, required this.onPressed, required this.title, this.color, this.style,});
+  final ShapeBorder? shapeBorder;
+  final EdgeInsetsGeometry? padding;
+  const CustomButton({ super.key, required this.onPressed, required this.title, this.color, this.shapeBorder, this.padding});
 
   @override
   Widget build(BuildContext context) {
 
-    return TextButton(
-        style: style,
+    return MaterialButton(
+      padding: padding,
+        height: 40,
+        color: color,
+        textColor: Colors.white,
+        shape: shapeBorder,
         onPressed: onPressed,
         child: Text(title));
     // custom button component to be used when necessary
