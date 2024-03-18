0d0a570344dd74ac3c88dbde61d65e14e7e382b6
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2024-03-18 02:40:47 +0000

    removed shape

--- lib/components/SubmitButton.dart
+++ lib/components/SubmitButton.dart
@@ -4,9 +4,8 @@ class CustomButton extends StatelessWidget {
   final VoidCallback onPressed;
   final String title;
   final Color? color;
-  final ShapeBorder? shapeBorder;
   final EdgeInsetsGeometry? padding;
-  const CustomButton({ super.key, required this.onPressed, required this.title, this.color, this.shapeBorder, this.padding});
+  const CustomButton({ super.key, required this.onPressed, required this.title, this.color, this.padding});
 
   @override
   Widget build(BuildContext context) {
@@ -16,7 +15,6 @@ class CustomButton extends StatelessWidget {
         height: 40,
         color: color,
         textColor: Colors.white,
-        shape: shapeBorder,
         onPressed: onPressed,
         child: Text(title));
     // custom button component to be used when necessary
