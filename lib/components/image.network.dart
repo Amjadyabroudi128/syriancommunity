ba20c5d3012c339b39ed2ed18a372ac274d22320
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2024-03-22 01:34:29 +0000

    image component

---
+++ lib/components/image.network.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/cupertino.dart';
+
+class myImage extends StatelessWidget {
+ final String? src;
+ final double? height;
+ final double? weight;
+  const myImage({super.key,  this.src, this.height, this.weight});
+
+  @override
+  Widget build(BuildContext context) {
+    return Image.network(
+      src!,
+      width: MediaQuery.of(context).size.width,
+      height: MediaQuery.of(context).size.height * 0.40,
+    );
+  }
+}
