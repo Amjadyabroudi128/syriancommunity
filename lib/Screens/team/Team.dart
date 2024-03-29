e0c315936afe83a9918b763ad9f9cf7a5bd66adc
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2024-03-29 02:18:50 +0000

    image height and width

--- lib/Screens/team/Team.dart
+++ lib/Screens/team/Team.dart
@@ -1,7 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:syrianadmin/classes/pickImage.dart';
 import 'package:syrianadmin/components/Sizedbox.dart';
 import 'package:syrianadmin/components/SubmitButton.dart';
 import 'package:syrianadmin/components/image.network.dart';
