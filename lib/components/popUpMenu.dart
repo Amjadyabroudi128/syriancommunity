506df071a3559bbdc7014524cefc8d149de665cc
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2024-04-03 03:25:14 +0100

    animation style

--- lib/components/popUpMenu.dart
+++ lib/components/popUpMenu.dart
@@ -3,13 +3,15 @@ import 'package:flutter/material.dart';
 class MyPopUpMenu extends StatelessWidget {
   final PopupMenuItemBuilder itemBuilder;
   final PopupMenuItemSelected onSelected;
-  const MyPopUpMenu({super.key, required this.itemBuilder, required this.onSelected, });
+  final AnimationStyle? popUpAnimationStyle;
+  const MyPopUpMenu({super.key, required this.itemBuilder, required this.onSelected, this.popUpAnimationStyle, });
 
   @override
   Widget build(BuildContext context) {
     return PopupMenuButton(
       itemBuilder: itemBuilder,
       onSelected: onSelected,
+      popUpAnimationStyle: popUpAnimationStyle,
     );
   }
 }
