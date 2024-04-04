8fe6751c0500cddbedc0ea71d7020da50b654e4c
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2024-04-04 01:25:47 +0100

    child

--- lib/components/Sizedbox.dart
+++ lib/components/Sizedbox.dart
@@ -3,13 +3,15 @@ import 'package:flutter/material.dart';
 class sizedBox extends StatelessWidget {
   final double? height;
   final double? width;
-   const sizedBox({super.key, this.height, this.width});
+  final Widget? child;
+   const sizedBox({super.key, this.height, this.width, this.child});
 
   @override
   Widget build(BuildContext context) {
   return  SizedBox(
     height: height,
     width: width ,
+    child: child,
   );
   }
 
