92681977c1cf36a256c2ab04ceb9a15a0631b49b
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2024-04-05 01:44:21 +0100

    body padding

--- lib/Register/LogIn.dart
+++ lib/Register/LogIn.dart
@@ -4,6 +4,7 @@ import 'package:syrianadmin/Cubits/auth_cubit.dart';
 import 'package:syrianadmin/components/Sizedbox.dart';
 import 'package:syrianadmin/components/TextField.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:syrianadmin/components/padding.dart';
 import 'package:syrianadmin/core/themes/fontSize.dart';
 class Login extends StatefulWidget {
   const Login({Key? key}) : super(key: key);
@@ -32,7 +33,7 @@ class _LoginState extends State<Login> {
       appBar: AppBar(
         title: Text(AppLocalizations.of(context)!.login),
       ),
-      body: Center(
+      body: padding(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
