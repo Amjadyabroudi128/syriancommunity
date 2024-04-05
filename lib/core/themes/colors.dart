dc1db79cc5c4066e055a3e0af0900d2f3dd1b182
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2024-04-05 01:43:57 +0100

    border color

--- lib/core/themes/colors.dart
+++ lib/core/themes/colors.dart
@@ -6,6 +6,7 @@ class ColorManager {
   static Color buttonColor = Color.fromARGB(255, 33, 173, 168);
   static Color dateColor = Color(0xFF9E9E9E);
   static Color hintText = Color(0xFF000000);
+  static Color borderColor = Color(0xFF000000);
   static Color fbColor = Color(0xFF0D47A1);
   static Color? addEdit = Color(0xFF757575);
   static Color delete = Color(0xFFD32F2F);
