85868349f561bdc17f681bd9f782922c2270a98c
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2024-04-13 01:48:21 +0100

    icon

--- lib/components/popUpMenu.dart
+++ lib/components/popUpMenu.dart
@@ -4,11 +4,13 @@ class MyPopUpMenu extends StatelessWidget {
   final PopupMenuItemBuilder itemBuilder;
   final PopupMenuItemSelected onSelected;
   final AnimationStyle? popUpAnimationStyle;
-  const MyPopUpMenu({super.key, required this.itemBuilder, required this.onSelected, this.popUpAnimationStyle, });
+  final Widget? icon;
+  const MyPopUpMenu({super.key, required this.itemBuilder, required this.onSelected, this.popUpAnimationStyle, this.icon, });
 
   @override
   Widget build(BuildContext context) {
     return PopupMenuButton(
+      icon: icon,
       itemBuilder: itemBuilder,
       onSelected: onSelected,
       popUpAnimationStyle: popUpAnimationStyle,
