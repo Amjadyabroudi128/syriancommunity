63dbf8ecffedcdb8b4f064262ca5a13ac3ec2cc9
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2024-04-16 15:47:58 +0100

    add celebration state

---
+++ lib/Cubits/add_celebration_state.dart
@@ -0,0 +1,10 @@
+part of 'add_celebration_cubit.dart';
+
+@immutable
+abstract class AddCelebrationState {}
+
+ class AddCelebrationInitial extends AddCelebrationState {}
+
+ class AddLoading extends AddCelebrationState {}
+
+ class AddSuccess extends AddCelebrationState {}
