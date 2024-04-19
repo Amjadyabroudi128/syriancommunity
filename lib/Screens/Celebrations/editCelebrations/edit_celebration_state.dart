4a66f0fda96405707f82564bfe0fc59ce9af9d9c
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2024-04-19 01:36:42 +0100

    edit celebration state

---
+++ lib/Screens/Celebrations/editCelebrations/edit_celebration_state.dart
@@ -0,0 +1,8 @@
+part of 'edit_celebration_cubit.dart';
+
+@immutable
+abstract class EditCelebrationState {}
+
+ class EditCelebrationInitial extends EditCelebrationState {}
+ class EditSuccess extends EditCelebrationState {}
+ class EditLoading extends EditCelebrationState {}
