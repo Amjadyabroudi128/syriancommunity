92f184c947999473759c63146797565d11a0ed13
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2024-04-19 02:11:26 +0100

    edit celebration cubit

--- lib/Screens/Celebrations/editCelebrations/edit_celebration_cubit.dart
+++ lib/Screens/Celebrations/editCelebrations/edit_celebration_cubit.dart
@@ -1,8 +1,16 @@
 import 'package:bloc/bloc.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:meta/meta.dart';
 
 part 'edit_celebration_state.dart';
 
 class EditCelebrationCubit extends Cubit<EditCelebrationState> {
   EditCelebrationCubit() : super(EditCelebrationInitial());
+
+  final CollectionReference celebration = FirebaseFirestore.instance.collection("Celebrations");
+
+  Future <void> EditCelebration(String DocID, Map<String, dynamic> newData) async {
+    await celebration.doc(DocID).update(newData);
+    emit(EditSuccess());
+  }
 }
