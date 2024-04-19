0233b55c4c78edb14146e13724fdb423f5a6679c
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2024-04-19 02:33:06 +0100

    cubit delete

---
+++ lib/Screens/Celebrations/Delete/delete_cubit.dart
@@ -0,0 +1,15 @@
+import 'package:bloc/bloc.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:meta/meta.dart';
+
+part 'delete_state.dart';
+
+class DeleteCubit extends Cubit<DeleteState> {
+  DeleteCubit() : super(DeleteInitial());
+  final CollectionReference celebration = FirebaseFirestore.instance.collection("Celebrations");
+
+  Future <void> delete(String DocID,) async {
+    celebration.doc(DocID).delete();
+    emit(DeleteSuccess());
+  }
+}
