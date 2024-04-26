0892cc1a5cc454e46c7ebbb8498ddbe4f2504354
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2024-04-26 02:21:27 +0100

    dismiss  keyboard

--- lib/Screens/HomePage/AddHomePage.dart
+++ lib/Screens/HomePage/AddHomePage.dart
@@ -32,77 +32,81 @@ class _AddInfoState extends State<AddInfo> {
   Widget build(BuildContext context) {
     final CollectionReference home =
     FirebaseFirestore.instance.collection('home');
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(AppLocalizations.of(context)!.addDetails),
-      ),
-      body: Center(
-        child: SingleChildScrollView(
-          child: padding(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                padding(child: Text(AppLocalizations.of(context)!.name)),
+    return GestureDetector(
+      onTap: () => FocusScope.of(context).requestFocus( FocusNode()),
+
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text(AppLocalizations.of(context)!.addDetails),
+        ),
+        body: Center(
+          child: SingleChildScrollView(
+            child: padding(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  padding(child: Text(AppLocalizations.of(context)!.name)),
+                    CustomTextForm(
+                        hinttext: "name",
+                        myController: name,
+                        suffixIcon: name.text.isEmpty ? null :
+                        IconButton(onPressed: name.clear, icon: Icon(Icons.clear), color: Colors.black,)
+
+                    ),
+                  sizedBox(),
+                  padding(child: Text(AppLocalizations.of(context)!.details,)),
                   CustomTextForm(
-                      hinttext: "name",
-                      myController: name,
-                      suffixIcon: name.text.isEmpty ? null :
-                      IconButton(onPressed: name.clear, icon: Icon(Icons.clear), color: Colors.black,)
+                      hinttext: "details",
+                      myController: details,
+                    maxLines: 6,
+                      suffixIcon: details.text.isEmpty ? null : IconButton(onPressed: details.clear, icon: Icon(Icons.clear), color: Colors.black,)
 
                   ),
-                sizedBox(),
-                padding(child: Text(AppLocalizations.of(context)!.details,)),
-                CustomTextForm(
-                    hinttext: "details",
-                    myController: details,
-                  maxLines: 6,
-                    suffixIcon: details.text.isEmpty ? null : IconButton(onPressed: details.clear, icon: Icon(Icons.clear), color: Colors.black,)
-
-                ),
-                sizedBox(height: 10,),
-                   Row(
-                     mainAxisAlignment: MainAxisAlignment.center,
-                     children: [
-                       CustomButton(
-                         onPressed: () async {
-                           if (name == null) {
-                             await home.doc().set(
-                               {
-                               "details": details.text,
-                               "time" : today
-                               }
-                             );
-                           } else if (details == null){
-                                 await home.doc().set(
+                  sizedBox(height: 10,),
+                     Row(
+                       mainAxisAlignment: MainAxisAlignment.center,
+                       children: [
+                         CustomButton(
+                           onPressed: () async {
+                             if (name == null) {
+                               await home.doc().set(
+                                 {
+                                 "details": details.text,
+                                 "time" : today
+                                 }
+                               );
+                             } else if (details == null){
+                                   await home.doc().set(
+                                     {
+                                     "name" : name.text,
+                                       "time" :today,
+                                     }
+                                   );
+                             } else {
+                               await home.doc().set(
                                    {
-                                   "name" : name.text,
+                                     "name" : name.text,
+                                     "details" : details.text,
                                      "time" :today,
                                    }
-                                 );
-                           } else {
-                             await home.doc().set(
-                                 {
-                                   "name" : name.text,
-                                   "details" : details.text,
-                                   "time" :today,
-                                 }
-                             );
-                             clearText();
-                             await FirebaseApi().initNotifications();
-                             await FirebaseMessaging.instance.subscribeToTopic("topic");
-                             Navigator.of(context).pushNamed("homepage");
-                           }
-                         },
-                            title: AppLocalizations.of(context)!.submit,
-                         color: (name.text.isEmpty)
-                             || (details.text.isEmpty) ? Colors.grey : ColorManager.submit
-                       ),
-                       sizedBox(width: 15,),
-                       cancelButton(),
-                     ],
-                   ),
+                               );
+                               clearText();
+                               await FirebaseApi().initNotifications();
+                               await FirebaseMessaging.instance.subscribeToTopic("topic");
+                               Navigator.of(context).pushNamed("homepage");
+                             }
+                           },
+                              title: AppLocalizations.of(context)!.submit,
+                           color: (name.text.isEmpty)
+                               || (details.text.isEmpty) ? Colors.grey : ColorManager.submit
+                         ),
+                         sizedBox(width: 15,),
+                         cancelButton(),
+                       ],
+                     ),
 
-              ],
+                ],
+              ),
             ),
           ),
         ),
