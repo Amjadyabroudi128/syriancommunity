aa7568257dfbc9325ede6f4b01472718bfd617b0
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2024-05-01 02:05:22 +0100

    remove unused colors

--- lib/core/themes/colors.dart
+++ lib/core/themes/colors.dart
@@ -3,13 +3,11 @@ import 'package:flutter/material.dart';
 class ColorManager {
   static Color specialGreen = Color.fromARGB(255, 33, 173, 168);
   static Color cardColor = Color(0xFFEEEEEE);
-  static Color buttonColor = Color.fromARGB(255, 33, 173, 168);
   static Color dateColor = Color(0xFF9E9E9E);
   static Color hintText = Color(0xFF000000);
   static Color borderColor = Color(0xFF000000);
   static Color fbColor = Color(0xFF0D47A1);
   static Color? addEdit = Color(0xFF757575);
   static Color delete = Color(0xFFD32F2F);
-  static Color expanstionTile = Color(0xFFEEEEEE);
   static Color submit =Color(0xFF4CAF50);
 }
