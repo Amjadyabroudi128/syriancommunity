06e88072ebbb2dadf42f5ff53ddec90df9df326d
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2024-05-01 02:05:38 +0100

    remove unused import

--- lib/Register/LogIn.dart
+++ lib/Register/LogIn.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:syrianadmin/Cubits/auth_cubit/auth_cubit.dart';
