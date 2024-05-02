4a6dfc4440d4cdc7dd9deffc4cb3e148e3db0e35
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2024-05-03 00:37:35 +0100

    scroll behaviour

--- lib/Screens/team/Team.dart
+++ lib/Screens/team/Team.dart
@@ -37,137 +37,140 @@ class _MeetOurTeamState extends State<MeetOurTeam> {
       ),
       body: Padding(
         padding: const EdgeInsets.all(12.0),
-        child: SingleChildScrollView(
-          child: Column(
-            children: [
-              padding(
-                child: user != null ? CustomButton(
-                  onPressed: (){
-                    Navigator.of(context).pushNamed("addMember");
-                  },
-                  title: AppLocalizations.of(context)!.addThings,
-                  color: ColorManager.addEdit,
-                ) : sizedBox(),
-              ),
-              StreamBuilder(
-                stream: members.snapshots(),
-                  builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
-                    if (snapshot.hasError) {
-                      return const Text('Something went wrong');
-                    }
-                    if (snapshot.connectionState == ConnectionState.waiting) {
-                      return Text("Loading");
-                    }
-                    if (snapshot.data!.docs.isEmpty) {
-                      return SafeArea(
-                        child: Center(
-                          child: Column(
-                            children: [
-                              sizedBox(height: 200),
-                              Text("nothing to see here yet", style: TextStyles.font20grey,)
-                            ],
-                          ),
-                        ),
-                      );
-                    }
-                    return SingleChildScrollView(
-                      child: ListView(
-                        physics: ScrollPhysics(),
-                        shrinkWrap: true,
-                        children: snapshot.data!.docs.map((DocumentSnapshot document){
-                        Map<String, dynamic> data = document.data()! as Map<String, dynamic>;
-                        return Column(
-                          children: [
-                          Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: user != null ? GestureDetector(
-                              child: Card(
-                                shape: const CircleBorder(),
-                                clipBehavior: Clip.antiAlias,
-                                child: data["image"] !=  null ? myImage(
-                                  src: data["image"],
-                                  height: 240,
-                                  width: 240,
-                                  fit: BoxFit.cover,
-                                ) : SizedBox.shrink(),
+        child: ScrollConfiguration(
+          behavior: ScrollBehavior(),
+          child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  padding(
+                    child: user != null ? CustomButton(
+                      onPressed: (){
+                        Navigator.of(context).pushNamed("addMember");
+                      },
+                      title: AppLocalizations.of(context)!.addThings,
+                      color: ColorManager.addEdit,
+                    ) : sizedBox(),
+                  ),
+                  StreamBuilder(
+                    stream: members.snapshots(),
+                      builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+                        if (snapshot.hasError) {
+                          return const Text('Something went wrong');
+                        }
+                        if (snapshot.connectionState == ConnectionState.waiting) {
+                          return Text("Loading");
+                        }
+                        if (snapshot.data!.docs.isEmpty) {
+                          return SafeArea(
+                            child: Center(
+                              child: Column(
+                                children: [
+                                  sizedBox(height: 200),
+                                  Text("nothing to see here yet", style: TextStyles.font20grey,)
+                                ],
                               ),
-                              onTap: () {
-                                Navigator.of(context).push(
-                                    MaterialPageRoute(builder: (context) =>
-                                        EditMember(DocID: document.id ,
-                                          oldName: data["name"],
-                                          oldDetail: data["details"],
-                                          oldUrl: data["image"],)
-                                    ));
-                              },
-                            ) :
-                            Card(
-                              shape: const CircleBorder(),
-                              clipBehavior: Clip.antiAlias,
-                              child: data["image"] !=  null ? myImage(
-                                src: data["image"],
-                                height: 240,
-                                width: 240,
-                                fit: BoxFit.cover,
-                              ) : SizedBox.shrink(),
                             ),
-                          ),
-                            Text(data["name"], style: TextStyles.font14green,),
-                            sizedBox(height: 12,),
-                            SizedBox(
-                              width: MediaQuery.of(context).size.width,
-                              child: IntrinsicHeight(
-                                child: Card(
-                                  child: Column(
-                                    children: [
-                                      padding(child: Text(data["details"], style: TextStyles.font17,) ,),
-                                      Padding(
-                                        padding: const EdgeInsets.only(left: 310),
-                                        child: user != null ? MyPopUpMenu(
-                                          itemBuilder: (context){
-                                            return [
-                                              PopupMenuItem<int>(
-                                                  value: 0,
-                                                  child: Text(AppLocalizations.of(context)!.edit)
-                                              ),
-                                              PopupMenuItem<int>(
-                                                value: 1,
-                                                child: Text(AppLocalizations.of(context)!.delete, style: TextStyles.delete,),
-                                              ),
-                                            ];
-                                            },
-                                          onSelected:(value){
-                                            if(value == 0){
-                                        Navigator.of(context).push(
-                                            MaterialPageRoute(builder: (context) =>
-                                                EditMember(DocID: document.id ,
-                                                  oldName: data["name"],
-                                                  oldDetail: data["details"],
-                                                  oldUrl: data["image"],)
-                                        ));
-                               } else if(value == 1){
-                                members.doc(document.id).delete();
-                              }
-                            },
-                          ) : SizedBox(),
-
-                                      ),
-                                    ],
+                          );
+                        }
+                        return SingleChildScrollView(
+                          child: ListView(
+                            physics: ScrollPhysics(),
+                            shrinkWrap: true,
+                            children: snapshot.data!.docs.map((DocumentSnapshot document){
+                            Map<String, dynamic> data = document.data()! as Map<String, dynamic>;
+                            return Column(
+                              children: [
+                              Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: user != null ? GestureDetector(
+                                  child: Card(
+                                    shape: const CircleBorder(),
+                                    clipBehavior: Clip.antiAlias,
+                                    child: data["image"] !=  null ? myImage(
+                                      src: data["image"],
+                                      height: 240,
+                                      width: 240,
+                                      fit: BoxFit.cover,
+                                    ) : SizedBox.shrink(),
                                   ),
+                                  onTap: () {
+                                    Navigator.of(context).push(
+                                        MaterialPageRoute(builder: (context) =>
+                                            EditMember(DocID: document.id ,
+                                              oldName: data["name"],
+                                              oldDetail: data["details"],
+                                              oldUrl: data["image"],)
+                                        ));
+                                  },
+                                ) :
+                                Card(
+                                  shape: const CircleBorder(),
+                                  clipBehavior: Clip.antiAlias,
+                                  child: data["image"] !=  null ? myImage(
+                                    src: data["image"],
+                                    height: 240,
+                                    width: 240,
+                                    fit: BoxFit.cover,
+                                  ) : SizedBox.shrink(),
                                 ),
                               ),
-                            ),
-                          ],
+                                Text(data["name"], style: TextStyles.font14green,),
+                                sizedBox(height: 12,),
+                                SizedBox(
+                                  width: MediaQuery.of(context).size.width,
+                                  child: IntrinsicHeight(
+                                    child: Card(
+                                      child: Column(
+                                        children: [
+                                          padding(child: Text(data["details"], style: TextStyles.font17,) ,),
+                                          Padding(
+                                            padding: const EdgeInsets.only(left: 310),
+                                            child: user != null ? MyPopUpMenu(
+                                              itemBuilder: (context){
+                                                return [
+                                                  PopupMenuItem<int>(
+                                                      value: 0,
+                                                      child: Text(AppLocalizations.of(context)!.edit)
+                                                  ),
+                                                  PopupMenuItem<int>(
+                                                    value: 1,
+                                                    child: Text(AppLocalizations.of(context)!.delete, style: TextStyles.delete,),
+                                                  ),
+                                                ];
+                                                },
+                                              onSelected:(value){
+                                                if(value == 0){
+                                            Navigator.of(context).push(
+                                                MaterialPageRoute(builder: (context) =>
+                                                    EditMember(DocID: document.id ,
+                                                      oldName: data["name"],
+                                                      oldDetail: data["details"],
+                                                      oldUrl: data["image"],)
+                                            ));
+                                   } else if(value == 1){
+                                    members.doc(document.id).delete();
+                                  }
+                                }, popUpAnimationStyle: AnimationStyle(duration: Duration(milliseconds: 400)),
+                              ) : SizedBox(),
+
+                                          ),
+                                        ],
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            );
+                          },
+                          ).toList(),
+                          ),
                         );
                       },
-                      ).toList(),
-                      ),
-                    );
-                  },
-              )
-            ],
+                  )
+                ],
+              ),
+            ),
           ),
-        ),
       ),
     );
   }
