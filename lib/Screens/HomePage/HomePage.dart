e80fd2cf04d040fdfbb15ea023015a62b4fa0d69
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2024-05-05 02:18:44 +0100

    dark theme data

--- lib/Screens/HomePage/HomePage.dart
+++ lib/Screens/HomePage/HomePage.dart
@@ -32,6 +32,7 @@ class _HomePageState extends State<HomePage> {
 
     User? user = FirebaseAuth.instance.currentUser;
     bool isDark = false;
+
     return Scaffold(
       appBar: AppBar(
         leading: Builder(
@@ -47,6 +48,7 @@ class _HomePageState extends State<HomePage> {
           },
         ),
         title: Text(AppLocalizations.of(context)!.syrianCommunity),
+
       ),
       drawer: SideDrawer(),
       body: padding(
