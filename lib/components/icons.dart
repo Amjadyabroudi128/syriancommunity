a94549f7767de1b07bd1587349015e1bd79c76a4
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2024-05-08 18:31:56 +0100

    icons drawer

--- lib/components/icons.dart
+++ lib/components/icons.dart
@@ -1,8 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class myIcons {
-  static Icon someIcon = Icon(
-    Icons.arrow_back
-  );
-
+  static Icon goBack = Icon(Icons.arrow_back);
+  static Icon drawer = Icon(CupertinoIcons.list_bullet,);
  }
