738c71b73e792fccababf9f2fd6f48fe2b3c24e4
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2024-05-08 16:12:51 +0100

    drawer width

--- lib/Screens/SideDrawer.dart
+++ lib/Screens/SideDrawer.dart
@@ -24,123 +24,126 @@ class _SideDrawerState extends State<SideDrawer> {
 
   @override
   Widget build(BuildContext context) {
-    return Drawer(
-      child: ListView(
-        padding: EdgeInsets.zero,
-        children: <Widget>[
-        Containers.image,
-          MYlist(
-            title: Text(AppLocalizations.of(context)!.home),
-            leading: Icon(
-              CupertinoIcons.home,
-            ),
-            onTap: (){
-              Navigator.of(context).pushReplacementNamed("homepage");
-            },
-          ),
-          MYlist(
-            title: Text(AppLocalizations.of(context)!.contact),
-            leading: Icon(
-              CupertinoIcons.mail_solid,
-            ),
-            onTap: (){
-              Navigator.pop(context);
-              Navigator.of(context).pushNamed("contactus");
-
-            },
-          ),
-          MYlist(
-            title: Text(AppLocalizations.of(context)!.team),
-            leading: Icon(
-              CupertinoIcons.group_solid,
-            ),
-            onTap: (){
-              Navigator.pop(context);
-              Navigator.of(context).pushNamed("ourteam");
-            },
-          ),
-          MYlist(
-            title: Text(AppLocalizations.of(context)!.communityResources),
-            leading: Icon(
-              CupertinoIcons.book_circle,
-            ),
-            onTap: (){
-              Navigator.pop(context);
-              Navigator.of(context).pushNamed("community");
-            },
-          ),
-          MYlist(
-            title: Text(AppLocalizations.of(context)!.celebrations),
-            leading: Icon(
-              Icons.celebration,
-            ),
-            onTap: (){
-              Navigator.pop(context);
-              Navigator.of(context).pushNamed("celebrations");
-            },
-          ),
-          StreamBuilder<User?>(
-            // The stream is the auth state changes from Firebase
-            stream: auth.authStateChanges(),
-            // The builder takes a snapshot of the stream data
-            builder: (context, snapshot) {
-              // If the snapshot has data, it means the user is signed in
-              if (snapshot.hasData) {
-                // Return the list tile for signed in users
-                return MYlist(
-                  leading: Icon(Icons.logout),
-                  title: Text(AppLocalizations.of(context)!.logout),
-                  onTap: (){
-                    auth.signOut();
-                    ScaffoldMessenger.of(context).showSnackBar
-                      (  SnackBar(content: Text(AppLocalizations.of(context)!.signedOut),));
-                    Navigator.of(context).pushNamed("homepage");
-                  },
-                );
-              } else {
-                // Return the list tile for signed out users
-                return MYlist(
-                  leading: Icon(Icons.login),
-                  title: Text(AppLocalizations.of(context)!.login),
-                  onTap: () {
-                    Navigator.push(context, CupertinoPageRoute<Widget>(
-                        builder: (BuildContext context) {
-                          return  Login();
-                    },
-                    )
-                    );
-
-                  },
-                );
-              }
-            },
-          ),
-
-          ExpansionTile(
+    return SizedBox(
+      width: MediaQuery.of(context).size.width * 0.69,
+      child: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+          Containers.image,
+            MYlist(
+              title: Text(AppLocalizations.of(context)!.home),
               leading: Icon(
-                CupertinoIcons.globe,
+                CupertinoIcons.home,
               ),
-              title: Text(AppLocalizations.of(context)!.language),
-
-              children: <Widget>[
-                MYlist(
-                  leading: Text("🇬🇧"),
-                  title: Text(AppLocalizations.of(context)!.english),
-                  onTap: (){
-                    MyApp.setLocale(context, Locale("en"));
-                  },
-                ),
-                MYlist(
-                  leading: Text("🇸🇦"),
-                  title: Text(AppLocalizations.of(context)!.arabic),
-                  onTap: (){
-                    MyApp.setLocale(context, Locale("ar"));
-                  },
-                ),
-
-              ],
+              onTap: (){
+                Navigator.of(context).pushReplacementNamed("homepage");
+              },
             ),
-        ],
+            MYlist(
+              title: Text(AppLocalizations.of(context)!.contact),
+              leading: Icon(
+                CupertinoIcons.mail_solid,
+              ),
+              onTap: (){
+                Navigator.pop(context);
+                Navigator.of(context).pushNamed("contactus");
+
+              },
+            ),
+            MYlist(
+              title: Text(AppLocalizations.of(context)!.team),
+              leading: Icon(
+                CupertinoIcons.group_solid,
+              ),
+              onTap: (){
+                Navigator.pop(context);
+                Navigator.of(context).pushNamed("ourteam");
+              },
+            ),
+            MYlist(
+              title: Text(AppLocalizations.of(context)!.communityResources),
+              leading: Icon(
+                CupertinoIcons.book_circle,
+              ),
+              onTap: (){
+                Navigator.pop(context);
+                Navigator.of(context).pushNamed("community");
+              },
+            ),
+            MYlist(
+              title: Text(AppLocalizations.of(context)!.celebrations),
+              leading: Icon(
+                Icons.celebration,
+              ),
+              onTap: (){
+                Navigator.pop(context);
+                Navigator.of(context).pushNamed("celebrations");
+              },
+            ),
+            StreamBuilder<User?>(
+              // The stream is the auth state changes from Firebase
+              stream: auth.authStateChanges(),
+              // The builder takes a snapshot of the stream data
+              builder: (context, snapshot) {
+                // If the snapshot has data, it means the user is signed in
+                if (snapshot.hasData) {
+                  // Return the list tile for signed in users
+                  return MYlist(
+                    leading: Icon(Icons.logout),
+                    title: Text(AppLocalizations.of(context)!.logout),
+                    onTap: (){
+                      auth.signOut();
+                      ScaffoldMessenger.of(context).showSnackBar
+                        (  SnackBar(content: Text(AppLocalizations.of(context)!.signedOut),));
+                      Navigator.of(context).pushNamed("homepage");
+                    },
+                  );
+                } else {
+                  // Return the list tile for signed out users
+                  return MYlist(
+                    leading: Icon(Icons.login),
+                    title: Text(AppLocalizations.of(context)!.login),
+                    onTap: () {
+                      Navigator.push(context, CupertinoPageRoute<Widget>(
+                          builder: (BuildContext context) {
+                            return  Login();
+                      },
+                      )
+                      );
+
+                    },
+                  );
+                }
+              },
+            ),
+
+            ExpansionTile(
+                leading: Icon(
+                  CupertinoIcons.globe,
+                ),
+                title: Text(AppLocalizations.of(context)!.language),
+
+                children: <Widget>[
+                  MYlist(
+                    leading: Text("🇬🇧"),
+                    title: Text(AppLocalizations.of(context)!.english),
+                    onTap: (){
+                      MyApp.setLocale(context, Locale("en"));
+                    },
+                  ),
+                  MYlist(
+                    leading: Text("🇸🇦"),
+                    title: Text(AppLocalizations.of(context)!.arabic),
+                    onTap: (){
+                      MyApp.setLocale(context, Locale("ar"));
+                    },
+                  ),
+
+                ],
+              ),
+          ],
+        ),
       ),
     );
 
