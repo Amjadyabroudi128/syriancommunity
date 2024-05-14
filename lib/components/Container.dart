1786178e46b3a78dcc94e8ff4651d8a355ae2f48
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2024-05-14 15:54:17 +0100

    add custom icon

--- lib/components/Container.dart
+++ lib/components/Container.dart
@@ -3,26 +3,9 @@ import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:syrianadmin/components/icons.dart';
 import 'package:syrianadmin/components/myImages.dart';
 class Containers {
-  static Container location = Container(
-    height: 300,
-    width: 370,
-    child: GoogleMap(
-      initialCameraPosition: CameraPosition(
-        target: LatLng(50.82062318563144, -0.12208351759729541),
-        zoom: 14,
-      ),
-      markers: {
-        Marker(
 
-            markerId: MarkerId("college"),
-            position: LatLng(50.82062318563144, -0.12208351759729541),
-            draggable: true,
-            onDragEnd: (value) {
-            }
-        )
-      },
-    ),
-  );
+
+  // static Container location =
   static Container image = Container(
     height: 250,
     decoration: BoxDecoration(
