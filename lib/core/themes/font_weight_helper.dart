17abd2910204f7d5c8bf3ffa6b74ac3566eca6d0
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2024-05-20 14:37:11 +0100

    remove font

--- lib/core/themes/font_weight_helper.dart
+++ lib/core/themes/font_weight_helper.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 class FontWeightHelper {
   static const FontWeight bold = FontWeight.w700;
-  static FontWeight semiBold = FontWeight.w600;
-  static FontWeight extraBold = FontWeight.w800;
   static FontWeight thin = FontWeight.w100;
 
 }
