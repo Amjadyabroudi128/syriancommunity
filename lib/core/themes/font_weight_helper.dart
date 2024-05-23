f8b55cbaed4b6af1a2f401458e2b95a733129fc5
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2024-05-23 14:26:36 +0100

    new font weight

--- lib/core/themes/font_weight_helper.dart
+++ lib/core/themes/font_weight_helper.dart
@@ -2,5 +2,6 @@ import 'package:flutter/material.dart';
 class FontWeightHelper {
   static const FontWeight bold = FontWeight.w700;
   static FontWeight thin = FontWeight.w100;
+  static FontWeight medium = FontWeight.w500;
 
 }
