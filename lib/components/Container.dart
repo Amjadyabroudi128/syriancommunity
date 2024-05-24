5d16c94a44369e9eca0707d4357799a9f2468077
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2024-05-24 14:13:58 +0100

    image icon

--- lib/components/Container.dart
+++ lib/components/Container.dart
@@ -1,13 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:syrianadmin/components/icons.dart';
-import 'package:syrianadmin/components/myImages.dart';
 class Containers {
   static Container image = Container(
     height: 250,
     decoration: BoxDecoration(
       image:  DecorationImage(
-        image: AssetImage(myImages.logo),
+        image: AssetImage(myIcons.logo),
         fit: BoxFit.cover,
       ),
     ),
