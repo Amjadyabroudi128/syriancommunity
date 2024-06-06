3e4ccb6117cac0f12ce5db64febb24cab4371a64
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2024-06-06 16:50:02 +0100

    if name empty add name, if details empty add details

--- lib/Screens/team/addTeam.dart
+++ lib/Screens/team/addTeam.dart
@@ -19,119 +19,158 @@ class AddMember extends StatefulWidget {
 }
 
 class _AddMemberState extends State<AddMember> {
- late final TextEditingController name = TextEditingController()..addListener(() {
-    setState(() {
+  late final TextEditingController name = TextEditingController()
+    ..addListener(() {
+      setState(() {});
     });
-  });
-  late final TextEditingController details = TextEditingController()..addListener(() {
-    setState(() {
+  late final TextEditingController details = TextEditingController()
+    ..addListener(() {
+      setState(() {});
     });
-  });
 
   final CollectionReference members =
-  FirebaseFirestore.instance.collection('members');
+      FirebaseFirestore.instance.collection('members');
   @override
   Widget build(BuildContext context) {
     String addThings = AppLocalizations.of(context)!.addThings;
     return GestureDetector(
-        onTap: () => FocusScope.of(context).requestFocus( FocusNode()),
-    child: Scaffold(
-      appBar: AppBar(
-        title: Text(addThings),
-      ),
-        body : Padding(
-          padding: EdgeInsets.all(20),
-          child: SingleChildScrollView(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                padding(child: Text(AppLocalizations.of(context)!.name, style: TextStyles.font14green,)),
-                sizedBox(height: 6,),
-                CustomTextForm(
-                  label: Text("Member's name "),
-                    myController: name,
-                    suffixIcon: name.text.isEmpty ? null : IconButton(onPressed: name.clear, icon: myIcons.clear,)
-                ),
-                sizedBox(height: 20,),
-                padding(child: Text(AppLocalizations.of(context)!.details, style: TextStyles.font14green,)),
-                sizedBox(height: 6,),
-                CustomTextForm(
-                  label: Text("Member's Details"),
-                    myController: details,
-                  suffixIcon: details.text.isEmpty ? null : IconButton(onPressed: details.clear, icon: myIcons.clear,)
-                ),
-                sizedBox(height: 12,),
-                Center(
-                  child: imageButton(),
-                ),
-                sizedBox(height: 15,),
-                   Row(
-                     mainAxisAlignment: MainAxisAlignment.center,
-                     children: [
-                       cancelButton(),
-                       sizedBox(width: 15,),
-                       addButton(),
-                     ],
-                   ),
-
-              ],
+        onTap: () => FocusScope.of(context).requestFocus(FocusNode()),
+        child: Scaffold(
+          appBar: AppBar(
+            title: Text(addThings),
+          ),
+          body: Padding(
+            padding: EdgeInsets.all(20),
+            child: SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  padding(
+                      child: Text(
+                    AppLocalizations.of(context)!.name,
+                    style: TextStyles.font14green,
+                  )),
+                  sizedBox(
+                    height: 6,
+                  ),
+                  CustomTextForm(
+                      label: Text("Member's name "),
+                      myController: name,
+                      suffixIcon: name.text.isEmpty
+                          ? null
+                          : IconButton(
+                              onPressed: name.clear,
+                              icon: myIcons.clear,
+                            )),
+                  sizedBox(
+                    height: 20,
+                  ),
+                  padding(
+                      child: Text(
+                    AppLocalizations.of(context)!.details,
+                    style: TextStyles.font14green,
+                  )),
+                  sizedBox(
+                    height: 6,
+                  ),
+                  CustomTextForm(
+                      label: Text("Member's Details"),
+                      myController: details,
+                      suffixIcon: details.text.isEmpty
+                          ? null
+                          : IconButton(
+                              onPressed: details.clear,
+                              icon: myIcons.clear,
+                            )),
+                  sizedBox(
+                    height: 12,
+                  ),
+                  Center(
+                    child: imageButton(),
+                  ),
+                  sizedBox(
+                    height: 15,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      cancelButton(),
+                      sizedBox(
+                        width: 15,
+                      ),
+                      addButton(),
+                    ],
+                  ),
+                ],
+              ),
             ),
           ),
-        ),
-    )
-    );
+        ));
   }
+
   void clearText() {
     name.clear();
     details.clear();
   }
-  cancelButton () {
-    return CustomButton(onPressed: (){
-      Navigator.of(context).pop();
-    }, title: AppLocalizations.of(context)!.cancel, color: ColorManager.delete,);
+
+  cancelButton() {
+    return CustomButton(
+      onPressed: () {
+        Navigator.of(context).pop();
+      },
+      title: AppLocalizations.of(context)!.cancel,
+      color: ColorManager.delete,
+    );
   }
-  addButton () {
+
+  addButton() {
     return CustomButton(
         onPressed: () async {
-          if ( name.text.isEmpty || details.text.isEmpty ) {
-            ScaffoldMessenger.of(context).showSnackBar
-              ( SnackBar(content: Text(AppLocalizations.of(context)!.addThings),));
-          } else if ( url.url == null) {
-            await members.doc().set(
-                {
-                  "name": name.text,
-                  "details": details.text
-                }
-            );
-            ScaffoldMessenger.of(context).showSnackBar
-              ( SnackBar(content: Text(AppLocalizations.of(context)!.addedSuccessfully),));
+          if (name.text.isEmpty && details.text.isEmpty) {
+            ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+              content: Text(AppLocalizations.of(context)!.addThings),
+            ));
+          }
+          else if (name.text.isEmpty) {
+            ScaffoldMessenger.of(context)
+                .showSnackBar(SnackBar(content: Text("Add Member's Name")));
+          } else if (details.text.isEmpty) {
+            ScaffoldMessenger.of(context)
+                .showSnackBar(SnackBar(content: Text("Add Member's details")));
+          } else if (url.url == null) {
+            await members
+                .doc()
+                .set({"name": name.text, "details": details.text});
+            ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+              content: Text(AppLocalizations.of(context)!.addedSuccessfully),
+            ));
             Navigator.of(context).pop();
             clearText();
           } else {
-            await members.doc().set(
-                {
-                  "name" : name.text,
-                  "details" : details.text,
-                  "image" : url.url,
-                }
-            );
-            ScaffoldMessenger.of(context).showSnackBar
-              ( SnackBar(content: Text(AppLocalizations.of(context)!.addedSuccessfully),));
+            await members.doc().set({
+              "name": name.text,
+              "details": details.text,
+              "image": url.url,
+            });
+            ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+              content: Text(AppLocalizations.of(context)!.addedSuccessfully),
+            ));
             Navigator.of(context).pushNamed("ourteam");
             clearText();
           }
         },
         title: AppLocalizations.of(context)!.submit,
-        color: (name.text.isEmpty)
-            && (details.text.isEmpty) ? Colors.grey : ColorManager.submit);
+        color: (name.text.isEmpty) && (details.text.isEmpty)
+            ? Colors.grey
+            : ColorManager.submit);
   }
-  imageButton () {
+
+  imageButton() {
     return CustomButton(
       title: AppLocalizations.of(context)!.image,
       onPressed: () async {
         await url.pickImage();
-        setState(() {
-        });
+        setState(() {});
       },
       color: ColorManager.addEdit,
     );
