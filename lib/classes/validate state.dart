fea6b6204297185a46939c7243c87177b3ced673
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2024-06-11 17:00:02 +0100

    validate class

--- lib/classes/validate state.dart
+++ lib/classes/validate state.dart
@@ -1,8 +1,13 @@
 
   import 'package:flutter/cupertino.dart';
- class Validate {
-   static final formKey = GlobalKey<FormState>();
-    validating () {
-    return Validate.formKey.currentState!.validate();
-   }
- }
+  class Validate {
+    static final GlobalKey<FormState> formKey = GlobalKey<FormState>();
+    static validating() {
+      final formState = formKey.currentState;
+      if (formState != null) {
+        return formState.validate();
+      }
+      return false;
+    }
+  }
+
