72e1046814fb62be713760ba9892c6a67f2aa7a8
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2024-06-12 14:56:15 +0100

    paddings between btns

--- lib/Screens/Celebrations/Add/AddCelebration.dart
+++ lib/Screens/Celebrations/Add/AddCelebration.dart
@@ -82,20 +82,19 @@ class _AddCelebrationState extends State<AddCelebration> {
                       return null;
                     },
                       ),
-                  sizedBox(height: 5,),
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: imageButton()
-                  ),
                    sizedBox(height: 5,),
                    Row(
                      mainAxisAlignment: MainAxisAlignment.center,
                      children: [
                        cancelButton(),
-                       sizedBox(width: 20,),
+                       sizedBox(width: 15,),
                        addButton(),
                      ],
                    ),
+                  Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: imageButton()
+                  ),
                 ],
               ),
             ),
