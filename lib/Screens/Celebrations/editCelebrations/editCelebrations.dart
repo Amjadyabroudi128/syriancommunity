545748edf74090e63957e8e21156c294b1694497
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2024-06-23 15:22:42 +0100

    wrapped the form instead of the whole page

--- lib/Screens/Celebrations/editCelebrations/editCelebrations.dart
+++ lib/Screens/Celebrations/editCelebrations/editCelebrations.dart
@@ -15,13 +15,20 @@ import 'package:syrianadmin/components/padding.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:syrianadmin/components/snackBar.dart';
 import 'package:syrianadmin/core/themes/colors.dart';
+
 class EditCelebration extends StatefulWidget {
   final String DocID;
   final String oldName;
   final String oldDetail;
   final String? oldUrl;
 
-  const EditCelebration({Key? key, required this.DocID, required this.oldName, required this.oldDetail, this.oldUrl}) : super(key: key);
+  const EditCelebration(
+      {Key? key,
+      required this.DocID,
+      required this.oldName,
+      required this.oldDetail,
+      this.oldUrl})
+      : super(key: key);
 
   @override
   State<EditCelebration> createState() => _EditCelebrationState();
@@ -37,23 +44,14 @@ class _EditCelebrationState extends State<EditCelebration> {
     celebrationDetails.text = widget.oldDetail;
     url.url = widget.oldUrl;
   }
+
   final CollectionReference celebrations =
-  FirebaseFirestore.instance.collection('Celebrations');
+      FirebaseFirestore.instance.collection('Celebrations');
   @override
   Widget build(BuildContext context) {
-    return BlocConsumer<EditCelebrationCubit, EditCelebrationState>(
-  listener: (context, state) {
-    if(state is EditSuccess) {
-      Navigator.pop(context);
-      showSnackBar(context, AppLocalizations.of(context)!.editedSuccessfully);
-    } else {
-      print("loading still");
-    }
-  },
-  builder: (context, state) {
     String title = AppLocalizations.of(context)!.editDetails;
     return GestureDetector(
-      onTap: () => FocusScope.of(context).requestFocus( FocusNode()),
+      onTap: () => FocusScope.of(context).requestFocus(FocusNode()),
       child: Scaffold(
         appBar: AppBar(
           title: Text(title),
@@ -63,64 +61,103 @@ class _EditCelebrationState extends State<EditCelebration> {
           child: SingleChildScrollView(
             child: Padding(
               padding: const EdgeInsets.all(12.0),
-              child: Form(
-                key: Validate.formKey,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    sizedBox(
-                      height: 20,
-                    ),
-                    Center(
-                      child: url.url != null ?myImage(
-                        onPressed: () async {
-                          await url.pickImage();
-                          setState(() {
-                          });
+              child: BlocListener<EditCelebrationCubit, EditCelebrationState>(
+                listener: (context, state) {
+                  if (state is EditSuccess) {
+                    Navigator.of(context).pushReplacementNamed("celebrations");
+                    showSnackBar(context, AppLocalizations.of(context)!.addedSuccessfully);
+                  }
+                  else if ( state is EditFailure){
+                    showSnackBar(context, state.errMessage);
+                  }
+                },
+                child: Form(
+                  key: Validate.formKey,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      sizedBox(
+                        height: 20,
+                      ),
+                      Center(
+                        child: url.url != null
+                            ? myImage(
+                                onPressed: () async {
+                                  await url.pickImage();
+                                  setState(() {});
+                                },
+                                src: url.url,
+                              )
+                            : SizedBox.shrink(),
+                      ),
+                      padding(
+                          child:
+                              Text(AppLocalizations.of(context)!.celebrations)),
+                      CustomTextForm(
+                        suffixIcon: celebrationName.text.isEmpty ||
+                                celebrationName == widget.oldName
+                            ? null
+                            : IconButton(
+                                onPressed: celebrationName.clear,
+                                icon: myIcons.clear,
+                              ),
+                        label:
+                            Text(AppLocalizations.of(context)!.celebrationName),
+                        myController: celebrationName,
+                        validator: (value) {
+                          if (value == null ||
+                              celebrationName.text == widget.oldName ||
+                              celebrationName.text.isEmpty) {
+                            return AppLocalizations.of(context)!.addThings;
+                          }
+                          return null;
                         },
-                        src: url.url,
-                      ) : SizedBox.shrink(),
-                    ),
-                    padding(child: Text(AppLocalizations.of(context)!.celebrations)),
-                    CustomTextForm(
-                      suffixIcon: celebrationName.text.isEmpty || celebrationName == widget.oldName ?
-                      null : IconButton(onPressed: celebrationName.clear, icon: myIcons.clear,),
-                      label: Text(AppLocalizations.of(context)!.celebrationName),
-                     myController: celebrationName,
-                      validator: (value) {
-                        if(value == null || celebrationName.text == widget.oldName || celebrationName.text.isEmpty) {
-                          return AppLocalizations.of(context)!.addThings;
-                        }
-                        return null;
-                      },),
-                    sizedBox(height: 7,),
-                    padding(child: Text(AppLocalizations.of(context)!.details)),
-                    sizedBox(height: 4,),
-                    CustomTextForm(
-                      suffixIcon: celebrationDetails.text.isEmpty || celebrationDetails == widget.oldDetail ?
-                      null : IconButton(onPressed: celebrationDetails.clear, icon: myIcons.clear,),
-                      label: Text(AppLocalizations.of(context)!.whatWeDo),
-                     myController: celebrationDetails,
-                      validator: (value) {
-                        if(value == null || celebrationDetails.text == widget.oldDetail || celebrationDetails.text.isEmpty) {
-                          return AppLocalizations.of(context)!.addThings;
-                        }
-                        return null;
-                      },
-                    ),
-                    sizedBox(height: 5,),
-                     Row(
-                       mainAxisAlignment: MainAxisAlignment.center,
-                       children: [
-                         cancelButton(),
-                         sizedBox(width: 10,),
-                         editButton(),
-                       ],
-                     ),
-                    Center(
-                      child: imageButton(),
-                    )
-                  ],
+                      ),
+                      sizedBox(
+                        height: 7,
+                      ),
+                      padding(
+                          child: Text(AppLocalizations.of(context)!.details)),
+                      sizedBox(
+                        height: 4,
+                      ),
+                      CustomTextForm(
+                        suffixIcon: celebrationDetails.text.isEmpty ||
+                                celebrationDetails == widget.oldDetail
+                            ? null
+                            : IconButton(
+                                onPressed: celebrationDetails.clear,
+                                icon: myIcons.clear,
+                              ),
+                        label: Text(AppLocalizations.of(context)!.whatWeDo),
+                        myController: celebrationDetails,
+                        validator: (value) {
+                          if (value == null ||
+                              celebrationDetails.text == widget.oldDetail ||
+                              celebrationDetails.text.isEmpty) {
+                            return AppLocalizations.of(context)!.addThings;
+                          }
+                          return null;
+                        },
+                      ),
+                      sizedBox(
+                        height: 5,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          cancelButton(),
+                          sizedBox(
+                            width: 10,
+                          ),
+                          editButton(),
+                        ],
+                      ),
+                      Center(
+                        child: imageButton(),
+                      )
+                    ],
+                  ),
                 ),
               ),
             ),
@@ -128,46 +165,48 @@ class _EditCelebrationState extends State<EditCelebration> {
         ),
       ),
     );
-  },
-);
   }
-  imageButton () {
-    return
-      CustomButton(
-        title: AppLocalizations.of(context)!.image,
-        onPressed: () async {
-          await url.pickImage();
-          setState(() {
-          });
-        },
-        color: ColorManager.addEdit,
-      );
-  }
-  cancelButton () {
+
+  imageButton() {
     return CustomButton(
-      onPressed: (){
+      title: AppLocalizations.of(context)!.image,
+      onPressed: () async {
+        await url.pickImage();
+        setState(() {});
+      },
+      color: ColorManager.addEdit,
+    );
+  }
+
+  cancelButton() {
+    return CustomButton(
+      onPressed: () {
         Navigator.pop(context);
       },
       title: AppLocalizations.of(context)!.cancel,
       color: ColorManager.delete,
     );
   }
-  editButton () {
+
+  editButton() {
     return CustomButton(
       onPressed: () async {
-        if (celebrationName.text == widget.oldName && celebrationDetails.text == widget.oldDetail
-            || celebrationName.text.isEmpty || celebrationDetails.text.isEmpty) {
+        if (celebrationName.text == widget.oldName &&
+                celebrationDetails.text == widget.oldDetail ||
+            celebrationName.text.isEmpty ||
+            celebrationDetails.text.isEmpty) {
           Validate.validating();
           showSnackBar(context, AppLocalizations.of(context)!.addThings);
         } else {
-            context.read<EditCelebrationCubit>().EditCelebration(widget.DocID,
-                {
-                  "name": celebrationName.text,
-                  "details": celebrationDetails.text,
-                  "image": url.url
-                });
+          context.read<EditCelebrationCubit>().EditCelebration(widget.DocID, {
+            "name": celebrationName.text,
+            "details": celebrationDetails.text,
+            "image": url.url
+          });
         }
       },
-      title: AppLocalizations.of(context)!.update, color: ColorManager.submit,);
+      title: AppLocalizations.of(context)!.update,
+      color: ColorManager.submit,
+    );
   }
 }
