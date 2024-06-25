d91d0c621f9bf210593a52bd92a56d6196250998
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2024-06-25 15:01:39 +0100

    fixed auth cubit

--- lib/Cubits/auth_cubit/auth_cubit.dart
+++ lib/Cubits/auth_cubit/auth_cubit.dart
@@ -6,20 +6,41 @@ part 'auth_state.dart';
 class AuthCubit extends Cubit<AuthState> {
   AuthCubit() : super(AuthInitial());
 
-  Future<void> login({required var email, required var password}) async {
+  // Future login({required var email, required var password}) async {
+  //   emit(LoginLoading());
+  //   try {
+  //     await FirebaseAuth.instance.signInWithEmailAndPassword(
+  //         email: email, password: password);
+  //     emit(LoginSuccess());
+  //   } on FirebaseAuthException catch (ex) {
+  //     if(ex.code == "user not found") {
+  //       emit(LoginFailed(errMessage: 'wrong email'));
+  //     } else if (ex.code == "wrong password") {
+  //       emit(LoginFailed(errMessage: 'wrong password'));
+  //     }
+  //   } catch (e) {
+  //     emit(LoginFailed(errMessage: 'something not right '));
+  //   }
+  // }
+  Future<void> login({required String email, required String password}) async {
     emit(LoginLoading());
     try {
       await FirebaseAuth.instance.signInWithEmailAndPassword(
-          email: email, password: password);
+        email: email,
+        password: password,
+      );
       emit(LoginSuccess());
     } on FirebaseAuthException catch (ex) {
-      if(ex.code == "user not found") {
-        emit(LoginFailed());
-      } else if (ex.code == "wrong password") {
-        emit(LoginFailed());
+      if (ex.code == 'user-not-found') {
+        emit(LoginFailed(errMessage: 'Wrong email'));
+      } else if (ex.code == 'wrong-password') {
+        emit(LoginFailed(errMessage: 'Wrong password'));
+      } else {
+        emit(LoginFailed(errMessage: 'Authentication error: ${ex.message}'));
       }
     } catch (e) {
-      emit(LoginFailed());
+      emit(LoginFailed(errMessage: 'Something went wrong: ${e.toString()}'));
     }
   }
+
 }
