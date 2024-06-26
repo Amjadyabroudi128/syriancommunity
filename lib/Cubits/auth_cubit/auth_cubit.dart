335ed0fae17f59b2765fe32d780d5398881399d3
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2024-06-26 17:25:17 +0100

    wrong email localization

--- lib/Cubits/auth_cubit/auth_cubit.dart
+++ lib/Cubits/auth_cubit/auth_cubit.dart
@@ -1,11 +1,12 @@
 import 'package:bloc/bloc.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 part 'auth_state.dart';
 
 class AuthCubit extends Cubit<AuthState> {
   AuthCubit() : super(AuthInitial());
-  Future login({required String email, required String password}) async {
+  Future login({required String email, required String password, required BuildContext context }  ) async {
     emit(LoginLoading());
     try {
       await FirebaseAuth.instance.signInWithEmailAndPassword(
@@ -15,7 +16,7 @@ class AuthCubit extends Cubit<AuthState> {
       emit(LoginSuccess());
     } on FirebaseAuthException catch (ex) {
       if (ex.code == 'user-not-found') {
-        emit(LoginFailed(errMessage: 'Wrong email'));
+        emit(LoginFailed(errMessage: AppLocalizations.of(context)!.wrongEmail));
       } else if (ex.code == 'wrong-password') {
         emit(LoginFailed(errMessage: 'Wrong password'));
       } else {
