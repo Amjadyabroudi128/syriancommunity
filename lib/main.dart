280560890ae598efe91bddc34c9831a908342922
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2024-06-28 15:49:57 +0100

    removed cubit

--- lib/main.dart
+++ lib/main.dart
@@ -2,14 +2,13 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:syrianadmin/Bloc/auth_bloc.dart';
-import 'package:syrianadmin/Cubits/auth_cubit/auth_cubit.dart';
 import 'package:syrianadmin/Screens/Celebrations/Add/add_celebration_cubit.dart';
 import 'package:syrianadmin/Screens/Celebrations/Delete/delete_cubit.dart';
 import 'package:syrianadmin/Screens/Celebrations/editCelebrations/edit_celebration_cubit.dart';
 import 'package:syrianadmin/appRoute.dart';
 import 'package:syrianadmin/components/firebase_options.dart';
 import 'Api/Firebase_api.dart';
+import 'Register/Bloc/auth_bloc.dart';
 import 'Screens/HomePage/HomePage.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'core/themes/app_theme.dart';
@@ -55,8 +54,6 @@ class _MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     return MultiBlocProvider(
       providers: [
-        BlocProvider(
-          create: (context) => AuthCubit(),),
         BlocProvider(create: (context) => AddCelebrationCubit(),),
         BlocProvider(create: (context) => EditCelebrationCubit()),
         BlocProvider(create: (context) => DeleteCubit()),
