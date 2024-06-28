45f6fae56d46606a0077bd9784987cf4e6369564
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2024-06-28 15:25:01 +0100

    trying bloc

--- lib/Bloc/auth_event.dart
+++ lib/Bloc/auth_event.dart
@@ -2,3 +2,8 @@ part of 'auth_bloc.dart';
 
 @immutable
 sealed class AuthEvent {}
+class LoginEvent extends AuthEvent {
+  final String email;
+  final String password;
+  LoginEvent ({required this.email, required this.password});
+}
