5c787b5a1657a3bfa5de5c6e56a67124915c002a
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2024-06-29 15:19:07 +0100

    collection ref

--- lib/Screens/Celebrations/Delete/bloc/delete_bloc.dart
+++ lib/Screens/Celebrations/Delete/bloc/delete_bloc.dart
@@ -1,18 +1,19 @@
 import 'package:bloc/bloc.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:meta/meta.dart';
+import 'package:syrianadmin/core/FirebaseCollections.dart';
 
 part 'delete_event.dart';
 part 'delete_state.dart';
 
 class DeleteBloc extends Bloc<DeleteEvent, DeleteState> {
   DeleteBloc() : super(DeleteInitial()) {
-    final CollectionReference celebration = FirebaseFirestore.instance.collection("Celebrations");
+    dbColl.celebration;
     on<DeleteEvent>((event, emit) {
       if (event is DeleteCelebration) {
         emit(DeleteLoading());
         try {
-          celebration.doc(event.DocID).delete();
+          dbColl.celebration.doc(event.DocID).delete();
           emit(DeleteSuccess());
         } catch (e) {
           DeleteFailure(errMessage: "something is wrong ");
