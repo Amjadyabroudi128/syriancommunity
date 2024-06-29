3fdc6f4adb9ef1146368f818473a976a47d18d79
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2024-06-29 15:16:25 +0100

    delete event

--- lib/Screens/Celebrations/Delete/bloc/delete_event.dart
+++ lib/Screens/Celebrations/Delete/bloc/delete_event.dart
@@ -2,3 +2,7 @@ part of 'delete_bloc.dart';
 
 @immutable
 sealed class DeleteEvent {}
+class DeleteCelebration extends DeleteEvent {
+  final String DocID;
+  DeleteCelebration ({required this.DocID});
+}
