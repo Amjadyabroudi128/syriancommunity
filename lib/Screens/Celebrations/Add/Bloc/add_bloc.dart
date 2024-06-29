99b4ae0a5469185358b8b7ab2b0a79e92e554ee9
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2024-06-29 14:30:47 +0100

    logic code

--- lib/Screens/Celebrations/Add/Bloc/add_bloc.dart
+++ lib/Screens/Celebrations/Add/Bloc/add_bloc.dart
@@ -1,13 +1,44 @@
 import 'package:bloc/bloc.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:meta/meta.dart';
+import 'package:syrianadmin/classes/pickImage.dart';
 
 part 'add_event.dart';
 part 'add_state.dart';
 
 class AddBloc extends Bloc<AddEvent, AddState> {
   AddBloc() : super(AddInitial()) {
-    on<AddEvent>((event, emit) {
-      // TODO: implement event handler
+    final CollectionReference celebrations =
+    FirebaseFirestore.instance.collection('Celebrations');
+    on<AddEvent>((event, emit) async{
+      if (event is AddCelebration) {
+        try {
+          if (url == null) {
+            await celebrations.doc().set(
+                {
+                  "name": event.name,
+                  "details": event.details
+                }
+            ); emit(AddSuccess());
+          } else {
+            await celebrations.doc().set(
+                {
+                  "image": url,
+                  "name": event.name,
+                  "details": event.details
+                }
+            );   emit(AddSuccess());
+          }
+        } on FirebaseException catch (e){
+          if(e.code == "No name") {
+            emit(AddFailure(errMessage: "please add a name"));
+          } else if (e.code == "no Details") {
+            emit(AddFailure(errMessage: "pleasee add details"));
+          }
+        } catch (e) {
+          emit(AddFailure(errMessage: "something else happened: $e"));
+        }
+      }
     });
   }
 }
