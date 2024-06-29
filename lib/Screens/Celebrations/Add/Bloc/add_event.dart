41e3651838bb4cce008e679dc8f37b268b4f4c98
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2024-06-29 14:22:29 +0100

    add celebration event

--- lib/Screens/Celebrations/Add/Bloc/add_event.dart
+++ lib/Screens/Celebrations/Add/Bloc/add_event.dart
@@ -2,3 +2,8 @@ part of 'add_bloc.dart';
 
 @immutable
 sealed class AddEvent {}
+class AddCelebration {
+  final String name;
+  final String details;
+  AddCelebration ({required this.name, required this.details});
+}
