3405ac5b62f00d7106da9bc419280ba7f31b2cc7
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2024-06-30 14:15:17 +0100

    edit bloc

--- lib/Screens/Celebrations/editCelebrations/controller/edit_celebration_bloc.dart
+++ lib/Screens/Celebrations/editCelebrations/controller/edit_celebration_bloc.dart
@@ -1,13 +1,23 @@
 import 'package:bloc/bloc.dart';
 import 'package:meta/meta.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:syrianadmin/core/FirebaseCollections.dart';
 
 part 'edit_celebration_event.dart';
 part 'edit_celebration_state.dart';
 
 class EditCelebrationBloc extends Bloc<EditCelebrationEvent, EditCelebrationState> {
   EditCelebrationBloc() : super(EditCelebrationInitial()) {
-    on<EditCelebrationEvent>((event, emit) {
-      // TODO: implement event handler
+    on<EditCelebrationEvent>((event, emit) async {
+      if (event is editCelebration) {
+        emit(EditLoading());
+        try {
+          dbColl.celebration.doc(event.DocID).update(event.newData);
+          emit(EditSuccess());
+        } catch (e) {
+          emit(EditFailure(errMessage: "couldn't add things"));
+        }
+      }
     });
   }
 }
