021e39cf179e3a16b4c33774f71b65210cedd3a9
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2024-06-30 13:59:58 +0100

    edit celebration state

--- lib/Screens/Celebrations/editCelebrations/controller/edit_celebration_state.dart
+++ lib/Screens/Celebrations/editCelebrations/controller/edit_celebration_state.dart
@@ -4,3 +4,9 @@ part of 'edit_celebration_bloc.dart';
 sealed class EditCelebrationState {}
 
 final class EditCelebrationInitial extends EditCelebrationState {}
+class EditSuccess extends EditCelebrationState {}
+class EditLoading extends EditCelebrationState {}
+class EditFailure extends EditCelebrationState {
+  final String errMessage;
+  EditFailure ({required this.errMessage});
+}
