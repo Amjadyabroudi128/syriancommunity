5908a7679b92b773105416274a60382126f963c2
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2024-07-03 18:51:35 +0100

    screen utilities

--- lib/Screens/Celebrations/Add/screen/AddCelebration.dart
+++ lib/Screens/Celebrations/Add/screen/AddCelebration.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:syrianadmin/classes/pickImage.dart' as url;
 import 'package:syrianadmin/classes/validate%20state.dart';
 import 'package:syrianadmin/components/Sizedbox.dart';
@@ -39,89 +40,93 @@ class _AddCelebrationState extends State<AddCelebration> {
           icon: myIcons.goBack);
     }
     String appBarTitle = AppLocalizations.of(context)!.addCelebration;
-    return GestureDetector(
-      onTap: () => FocusScope.of(context).requestFocus(FocusNode()),
-      child: BlocListener<AddBloc, AddState>(
-    listener: (context, state) {
-      if (state is AddSuccess) {
-        Navigator.of(context).pushReplacementNamed("celebrations");
-        showSnackBar(context, AppLocalizations.of(context)!.addedSuccessfully);
-        clearText();
-      }
-      else if ( state is AddFailure){
-        showSnackBar(context, state.errMessage);
-      }
-    },
-        child: Scaffold(
-          appBar: AppBar(
-            leading: goBack(),
-            title: Text(appBarTitle),
-          ),
-          body: padding(
-            child: SingleChildScrollView(
-              child: Form(
-                key: Validate.formKey,
-                autovalidateMode: AutovalidateMode.onUserInteraction,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    padding(
-                        child:
-                        Text(AppLocalizations.of(context)!.celebrations)),
-                    CustomTextForm(
-                      myController: celebrationName,
-                      label:
-                      Text(AppLocalizations.of(context)!.celebrationName),
-                      suffixIcon: celebrationName.text.isEmpty
-                          ? null
-                          : IconButton(
-                        onPressed: celebrationName.clear,
-                        icon: myIcons.clear,
-                      ),
-                      validator: (value) {
-                        if (value == null || celebrationName.text.isEmpty) {
-                          return AppLocalizations.of(context)!.addCelebration;
-                        }
-                        return null;
-                      },
-                    ),
-                    sizedBox(),
-                    padding(
-                      child: Text(AppLocalizations.of(context)!.details),
-                    ),
-                    CustomTextForm(
-                      myController: celebrationDetail,
-                      label: Text(AppLocalizations.of(context)!.whatWeDo),
-                      suffixIcon: celebrationDetail.text.isEmpty
-                          ? null
-                          : IconButton(
-                        onPressed: celebrationDetail.clear,
-                        icon: myIcons.clear,
-                      ),
-                      validator: (value) {
-                        if (value == null || celebrationDetail.text.isEmpty) {
-                          return AppLocalizations.of(context)!.addDetails;
-                        }
-                        return null;
-                      },
-                    ),
-                    sizedBox(
-                      height: 5,
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        cancelButton(),
-                        sizedBox(
-                          width: 15,
+    return ScreenUtilInit(
+      minTextAdapt: true,
+      splitScreenMode: true,
+      child: GestureDetector(
+        onTap: () => FocusScope.of(context).requestFocus(FocusNode()),
+        child: BlocListener<AddBloc, AddState>(
+      listener: (context, state) {
+        if (state is AddSuccess) {
+          Navigator.of(context).pushReplacementNamed("celebrations");
+          showSnackBar(context, AppLocalizations.of(context)!.addedSuccessfully);
+          clearText();
+        }
+        else if ( state is AddFailure){
+          showSnackBar(context, state.errMessage);
+        }
+      },
+          child: Scaffold(
+            appBar: AppBar(
+              leading: goBack(),
+              title: Text(appBarTitle),
+            ),
+            body: padding(
+              child: SingleChildScrollView(
+                child: Form(
+                  key: Validate.formKey,
+                  autovalidateMode: AutovalidateMode.onUserInteraction,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      padding(
+                          child:
+                          Text(AppLocalizations.of(context)!.celebrations)),
+                      CustomTextForm(
+                        myController: celebrationName,
+                        label:
+                        Text(AppLocalizations.of(context)!.celebrationName),
+                        suffixIcon: celebrationName.text.isEmpty
+                            ? null
+                            : IconButton(
+                          onPressed: celebrationName.clear,
+                          icon: myIcons.clear,
                         ),
-                        addButton(),
-                      ],
-                    ),
-                    Center(
-                      child: imageButton(),
-                    )
-                  ],
+                        validator: (value) {
+                          if (value == null || celebrationName.text.isEmpty) {
+                            return AppLocalizations.of(context)!.addCelebration;
+                          }
+                          return null;
+                        },
+                      ),
+                      sizedBox(),
+                      padding(
+                        child: Text(AppLocalizations.of(context)!.details),
+                      ),
+                      CustomTextForm(
+                        myController: celebrationDetail,
+                        label: Text(AppLocalizations.of(context)!.whatWeDo),
+                        suffixIcon: celebrationDetail.text.isEmpty
+                            ? null
+                            : IconButton(
+                          onPressed: celebrationDetail.clear,
+                          icon: myIcons.clear,
+                        ),
+                        validator: (value) {
+                          if (value == null || celebrationDetail.text.isEmpty) {
+                            return AppLocalizations.of(context)!.addDetails;
+                          }
+                          return null;
+                        },
+                      ),
+                      sizedBox(
+                        height: 10.h,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          cancelButton(),
+                          sizedBox(
+                            width: 15.w,
+                          ),
+                          addButton(),
+                        ],
+                      ),
+                      Center(
+                        child: imageButton(),
+                      )
+                    ],
+                  ),
                 ),
               ),
             ),
