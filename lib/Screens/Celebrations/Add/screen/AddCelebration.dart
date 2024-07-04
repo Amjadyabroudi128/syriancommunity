911656d335836bdc16156f952a01cce0e7956f0f
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2024-07-04 17:41:11 +0100

    if state is error make suffix icon color red

--- lib/Screens/Celebrations/Add/screen/AddCelebration.dart
+++ lib/Screens/Celebrations/Add/screen/AddCelebration.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
@@ -29,6 +30,7 @@ class _AddCelebrationState extends State<AddCelebration> {
     ..addListener(() {
       setState(() {});
     });
+  bool textFieldError = false;
 
   @override
   Widget build(BuildContext context) {
@@ -69,23 +71,23 @@ class _AddCelebrationState extends State<AddCelebration> {
                       padding(
                           child:
                           Text(AppLocalizations.of(context)!.celebrations)),
-                      CustomTextForm(
-                        myController: celebrationName,
-                        label:
-                        Text(AppLocalizations.of(context)!.celebrationName),
-                        suffixIcon: celebrationName.text.isEmpty
-                            ? null
-                            : IconButton(
-                          onPressed: celebrationName.clear,
-                          icon: myIcons.clear,
+                       CustomTextForm(
+                          myController: celebrationName,
+                          label:
+                          Text(AppLocalizations.of(context)!.celebrationName),
+                          suffixIcon: celebrationName.text.isEmpty
+                              ? null
+                              : IconButton(
+                            onPressed: celebrationName.clear,
+                            icon: myIcons.clear,
+                          ),
+                          validator: (value) {
+                            if (value == null || celebrationName.text.isEmpty) {
+                              return AppLocalizations.of(context)!.addCelebration;
+                            }
+                            return null;
+                          },
                         ),
-                        validator: (value) {
-                          if (value == null || celebrationName.text.isEmpty) {
-                            return AppLocalizations.of(context)!.addCelebration;
-                          }
-                          return null;
-                        },
-                      ),
                       sizedBox(),
                       padding(
                         child: Text(AppLocalizations.of(context)!.details),
@@ -93,11 +95,20 @@ class _AddCelebrationState extends State<AddCelebration> {
                       CustomTextForm(
                         myController: celebrationDetail,
                         label: Text(AppLocalizations.of(context)!.whatWeDo),
-                        suffixIcon: celebrationDetail.text.isEmpty
-                            ? null
-                            : IconButton(
-                          onPressed: celebrationDetail.clear,
-                          icon: myIcons.clear,
+                        // suffixIcon: celebrationDetail.text.isEmpty
+                        //     ? null
+                        //     : IconButton(
+                        //   onPressed: celebrationDetail.clear,
+                        //   icon: myIcons.clear,
+                        // ),
+                        suffixIcon: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            Icon(
+                              CupertinoIcons.exclamationmark,
+                            )
+                          ],
                         ),
                         validator: (value) {
                           if (value == null || celebrationDetail.text.isEmpty) {
