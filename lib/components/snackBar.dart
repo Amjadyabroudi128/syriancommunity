177b9b218f254b42d17fd154ff2a12419996d5de
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2024-07-09 14:53:32 +0100

    added default duration to snackbar

--- lib/components/snackBar.dart
+++ lib/components/snackBar.dart
@@ -1,10 +1,11 @@
 
 import 'package:flutter/material.dart';
 
-void showSnackBar(BuildContext context, String message) {
+void showSnackBar(BuildContext context, String message, {Duration? duration}) {
   ScaffoldMessenger.of(context).showSnackBar(
     SnackBar(
       content: Text(message),
+      duration: duration ?? const Duration(seconds: 3),
     ),
   );
 }
