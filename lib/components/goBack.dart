df46fec9c2582c89e147fd00e1ded05ad3b17849
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2024-07-13 12:31:30 +0100

    icon default

--- lib/components/goBack.dart
+++ lib/components/goBack.dart
@@ -5,14 +5,13 @@ import 'icons.dart';
 
 class goBack extends StatelessWidget {
   final VoidCallback? onPressed;
-  final Widget? icon;
-  const goBack({super.key, this.onPressed, this.icon});
+  const goBack({super.key, this.onPressed});
 
   @override
   Widget build(BuildContext context) {
     return IconButton(
         onPressed: onPressed,
-        icon: icon!
+        icon: myIcons.goBack
     );
   }
 }
