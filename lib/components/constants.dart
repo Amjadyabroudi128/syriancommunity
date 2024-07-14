9facacd6796b46178b06f1558c3f5a58315a4b2a
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2024-07-14 14:58:45 +0100

    constants

---
+++ lib/components/constants.dart
@@ -0,0 +1,6 @@
+
+ import 'package:flutter/cupertino.dart';
+import 'package:syrianadmin/core/themes/fontSize.dart';
+
+ Text kNothing =  Text("nothing to see here yet :( ", style: TextStyles.font20grey,);
+ Text kLoading = Text("Loading");
