de3012e39ea8eaed06974eb410f456ce4955aa2b
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2024-07-18 18:23:29 +0100

    edit can not be empty

--- lib/Screens/team/EditTeam.dart
+++ lib/Screens/team/EditTeam.dart
@@ -157,10 +157,12 @@ class _EditMemberState extends State<EditMember> {
     String btnTitle = AppLocalizations.of(context)!.update;
     Color btnClr = (widget.oldName == name.text && widget.oldDetail == details.text ||
         name.text.isEmpty || details.text.isEmpty)  ? Colors.grey : ColorManager.submit;
+    bool isChangedorEmpty = (name.text == widget.oldName && details.text == widget.oldDetail) ||
+        name.text.isEmpty || details.text.isEmpty;
    return CustomButton(
         onPressed: () async {
           Validate.validating();
-          if(name.text == widget.oldName && details.text == widget.oldDetail) {
+          if(isChangedorEmpty) {
             setState(() {
               isUpdated = false;
               showSnackBar(context, AppLocalizations.of(context)!.addThings);
