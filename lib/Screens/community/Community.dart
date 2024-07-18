bb9322e0f066343a48b09f20da0d8de26a6db8a0
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2024-07-18 17:59:31 +0100

    only call it once

--- lib/Screens/community/Community.dart
+++ lib/Screens/community/Community.dart
@@ -31,144 +31,138 @@ class _CommunityState extends State<Community> {
   @override
   Widget build(BuildContext context) {
     User? user = FirebaseAuth.instance.currentUser;
-    return ScreenUtilInit(
-      minTextAdapt: true,
-      splitScreenMode: true,
-      child: Scaffold(
-        appBar: AppBar(
-          leading: goBack(
-            onPressed: (){
-              Navigator.of(context).pushNamed("homepage");
-            },
-          ),
-          title: Text(AppLocalizations.of(context)!.communityResources),
+    return Scaffold(
+      appBar: AppBar(
+        leading: goBack(
+          onPressed: (){
+            Navigator.of(context).pushNamed("homepage");
+          },
         ),
-        body: ScrollConfiguration(
-          behavior: ScrollBehavior(),
-          child: SingleChildScrollView(
-            child: Column(
-              children: [
-                padding(
+        title: Text(AppLocalizations.of(context)!.communityResources),
+      ),
+      body: ScrollConfiguration(
+        behavior: ScrollBehavior(),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Center(
+                  child: user != null ? addThings(): sizedBox()
+              ),
+              StreamBuilder(
+                stream: dbColl.community.snapshots(),
+                builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+              if (snapshot.hasError) {
+                return wrong;
+              }
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return CircularProgressIndicator();
+              }
+              if (snapshot.data!.docs.isEmpty) {
+                return SafeArea(
                   child: Center(
-                    child: user != null ? addThings(): sizedBox(),
+                    child: Column(
+                      children: [
+                        sizedBox(height: 200.h,),
+                        kNothing
+                      ],
+                    ),
                   ),
-                ),
-                StreamBuilder(
-                  stream: dbColl.community.snapshots(),
-                  builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
-                if (snapshot.hasError) {
-                  return wrong;
-                }
-                if (snapshot.connectionState == ConnectionState.waiting) {
-                  return CircularProgressIndicator();
-                }
-                if (snapshot.data!.docs.isEmpty) {
-                  return SafeArea(
-                    child: Center(
+                );
+              }
+              return SingleChildScrollView(
+                child: ListView(
+                  physics: ScrollPhysics(),
+                  shrinkWrap: true,
+                  children: snapshot.data!.docs.map((DocumentSnapshot document){
+                    Map<String, dynamic> data = document.data()! as Map<String, dynamic>;
+                    return Padding(
+                      padding: const EdgeInsets.all(8.0),
                       child: Column(
                         children: [
-                          sizedBox(height: 200,),
-                          kNothing
-                        ],
-                      ),
-                    ),
-                  );
-                }
-                return SingleChildScrollView(
-                  child: ListView(
-                    physics: ScrollPhysics(),
-                    shrinkWrap: true,
-                    children: snapshot.data!.docs.map((DocumentSnapshot document){
-                      Map<String, dynamic> data = document.data()! as Map<String, dynamic>;
-                      return Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Column(
-                          children: [
-                            padding(
-                              child: user != null ? Card(
-                                child: data["image"] != null ? myImage(
-                                  onPressed: () {
-                                      Navigator.of(context).push(
-                                          MaterialPageRoute(builder: (context) =>
-                                              EditCommunity(DocID: document.id ,
-                                                oldName: data["name"],
-                                                oldUrl: data["image"],
-                                                oldDetails: data["details"],)
-                                          ));
+                          padding(
+                            child: user != null ? Card(
+                              child: data["image"] != null ? myImage(
+                                onPressed: () {
+                                    Navigator.of(context).push(
+                                        MaterialPageRoute(builder: (context) =>
+                                            EditCommunity(DocID: document.id ,
+                                              oldName: data["name"],
+                                              oldUrl: data["image"],
+                                              oldDetails: data["details"],)
+                                        ));
 
-                                  },
-                                  src: data["image"],
+                                },
+                                src: data["image"],
+                                height: MediaQuery.of(context).size.height * 0.50.h,
+                              ) : SizedBox.shrink(),
+                            ) : Card(
+                              child: data["image"] != null ? myImage(
+                                src: data["image"],
                                   height: MediaQuery.of(context).size.height * 0.40.h,
-                                ) : SizedBox.shrink(),
-                              ) : Card(
-                                child: data["image"] != null ? myImage(
-                                  src: data["image"],
-                                    height: MediaQuery.of(context).size.height * 0.40.h,
-                                ) : SizedBox.shrink(),
-                              ),
+                              ) : SizedBox.shrink(),
                             ),
-                            Text(data["name"], style: TextStyles.font14green,),
-                            sizedBox(height: 12.h,),
-                            SizedBox(
-                              width: MediaQuery.of(context).size.width,
-                              child: IntrinsicHeight(
-                                child: Card(
-                                  child: Column(
-                                    children: [
-                                        padding(
-                                          child: Text(data["details"], style: TextStyles.font17,),
-                                        ),
-                                        Padding(
-                                          padding:  EdgeInsets.only(left: 310, ),
-                                          child: user != null ? MyPopUpMenu(
-                                            itemBuilder: (context) {
-                                            return [
-                                              PopupMenuItem<myPop>(
-                                                value: myPop.edit,
-                                                child: Text(AppLocalizations.of(context)!.edit),
-                                              ),
-                                              PopupMenuItem<myPop>(
-                                                value: myPop.delete,
-                                                child: Text(AppLocalizations.of(context)!.delete, style: TextStyles.delete,),
-                                              ),
-                                            ];
-                                            },
-                                              onSelected:(value){
-                                                if(value == myPop.edit){
-                                                  Navigator.of(context).push(
-                                                      CupertinoPageRoute(builder: (context) =>
-                                                          EditCommunity(DocID: document.id ,
-                                                            oldName: data["name"],
-                                                            oldUrl: data["image"],
-                                                            oldDetails: data["details"],)
-                                                      ));
-
-                                                }else if(value == myPop.delete){
-                                                  dbColl.community.doc(document.id).delete();
-                                                  showSnackBar(context, AppLocalizations.of(context)!.deleted);
-                                                }
-                                              },
-                                            popUpAnimationStyle: AnimationStyle(
-                                                duration: kDuration
+                          ),
+                          Text(data["name"], style: TextStyles.font14green,),
+                          sizedBox(height: 12.h,),
+                          SizedBox(
+                            width: MediaQuery.of(context).size.width,
+                            child: IntrinsicHeight(
+                              child: Card(
+                                child: Column(
+                                  children: [
+                                      padding(
+                                        child: Text(data["details"], style: TextStyles.font17,),
+                                      ),
+                                      Padding(
+                                        padding:  EdgeInsets.only(left: 310, ),
+                                        child: user != null ? MyPopUpMenu(
+                                          itemBuilder: (context) {
+                                          return [
+                                            PopupMenuItem<myPop>(
+                                              value: myPop.edit,
+                                              child: Text(AppLocalizations.of(context)!.edit),
                                             ),
-                                          ) : sizedBox(height: 40.h,),
-                                        ),
-                                    ],
-                                  ),
+                                            PopupMenuItem<myPop>(
+                                              value: myPop.delete,
+                                              child: Text(AppLocalizations.of(context)!.delete, style: TextStyles.delete,),
+                                            ),
+                                          ];
+                                          },
+                                            onSelected:(value){
+                                              if(value == myPop.edit){
+                                                Navigator.of(context).push(
+                                                    CupertinoPageRoute(builder: (context) =>
+                                                        EditCommunity(DocID: document.id ,
+                                                          oldName: data["name"],
+                                                          oldUrl: data["image"],
+                                                          oldDetails: data["details"],)
+                                                    ));
+
+                                              }else if(value == myPop.delete){
+                                                dbColl.community.doc(document.id).delete();
+                                                showSnackBar(context, AppLocalizations.of(context)!.deleted);
+                                              }
+                                            },
+                                          popUpAnimationStyle: AnimationStyle(
+                                              duration: kDuration
+                                          ),
+                                        ) : sizedBox(height: 40.h,),
+                                      ),
+                                  ],
                                 ),
                               ),
                             ),
-                          ],
-                        ),
-                      );
-                    },
-                    ).toList(),
-                  ),
-                  );
+                          ),
+                        ],
+                      ),
+                    );
                   },
-                )
-              ],
-            ),
+                  ).toList(),
+                ),
+                );
+                },
+              )
+            ],
           ),
         ),
       ),
