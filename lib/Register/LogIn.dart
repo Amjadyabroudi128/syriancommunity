ac353e5c0d6710de5e4ab2b24aec7217c1eb614d
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2024-07-20 16:21:01 +0100

    prefix icon color

--- lib/Register/LogIn.dart
+++ lib/Register/LogIn.dart
@@ -105,6 +105,7 @@ class _LoginState extends State<Login> {
                     keyboardType: TextInputType.visiblePassword,
                     label: Text(AppLocalizations.of(context)!.password),
                     prefixIcon: isEmpty ? locked: unLocked,
+                    prefixIconColor: isEmpty ? null : ColorManager.submit,
                     validator: (pass){
                        if (pass == null || testPassword.text.isEmpty){
                          return "\u2757 ${AppLocalizations.of(context)!.password}";
