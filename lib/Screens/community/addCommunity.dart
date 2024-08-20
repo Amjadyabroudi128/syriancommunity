e5fe9e0132e02fe37a90660c5f545046786c0165
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2024-08-20 15:36:31 +0100

    btn color

--- lib/Screens/community/addCommunity.dart
+++ lib/Screens/community/addCommunity.dart
@@ -119,8 +119,8 @@ class _addCommunityState extends State<addCommunity> {
   }
   submitButton() {
     bool isEmpty = (name.text.isEmpty || details.text.isEmpty);
-    Color btnColor = (name.text.isEmpty)
-        || (details.text.isEmpty) ? ColorManager.emptyLogin : ColorManager.submit;
+    Color? btnColor = (name.text.isEmpty)
+        || (details.text.isEmpty) ? empty : submit;
     String btnTitle = AppLocalizations.of(context)!.submit;
     return CustomButton(onPressed: () async {
       if (isEmpty) {
